183d3e0ea50a10bfed011e069d55dc06ea73c00e
Martin Kavík <martin@kavik.cz>
2020-10-19 23:19:40 +0200

    cx

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use wasm_bindgen::{prelude::*, JsCast};
 use moxie::{runtime::Runtime, state, Commit, Key};
 use std::cell::RefCell;
 
-type State<T> = (Commit<T>, Key<T>);
-
 const ELEMENT_ID: &str = "app";
 
 macro_rules! log {
@@ -14,6 +12,25 @@ macro_rules! log {
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(input: &str);
+}
+
+type State<T> = (Commit<T>, Key<T>);
+
+#[derive(Clone)]
+struct Cx {
+    state_node: State<Node>,
+}
+
+struct Node {
+    node_ws: web_sys::Node,
+}
+
+impl Drop for Node {
+    fn drop(&mut self) {
+        let parent = self.node_ws.parent_node().expect("parent Node");
+        parent.remove_child(&self.node_ws);
+        log!("Node dropped");
+    }
 }
 
 thread_local! {
@@ -41,7 +58,7 @@ fn main() {
     console_error_panic_hook::set_once();
 
     runtime_run_once();
-    runtime_run_once();
+    // runtime_run_once();
 }
 
 #[topo::nested]
@@ -51,61 +68,93 @@ fn root() {
     let state_node = state(|| Node {
         node_ws: web_sys::Node::from(document().get_element_by_id(ELEMENT_ID).expect("root element"))
     });
+    let cx = Cx { state_node };
 
-    let (first_run, first_run_key) = state(|| true);
-    if *first_run {
-        first_run_key.set(false);
-        panel(|state_node| {
-            text("Panel 1", &state_node);
-            text("A", &state_node);
-        }, &state_node);
-    }
-    panel(|state_node| {
-        text("Panel 2", &state_node);
-        text("B", &state_node);
-    }, &state_node);
+    row(cx.clone(), |cx| {
+        column(cx.clone(), |cx| {
+            el( cx.clone(), |cx| {
+                text(cx.clone(), "A1"); 
+            });
+            el(cx.clone(), |cx| { 
+                text(cx.clone(), "A2");
+            });
+        });
+        column(cx.clone(), |cx| {
+            el(cx.clone(), |cx| {
+                text(cx.clone(), "B1");
+            });
+            el(cx.clone(), |cx| { 
+                text(cx.clone(), "B2");
+            });
+        });
+    });
+
+    // let (first_run, first_run_key) = state(|| true);
+    // if *first_run {
+    //     first_run_key.set(false);
+    //     panel(|cx| {
+    //         text("Panel 1", cx.clone());
+    //         text("A", cx.clone());
+    //     }, cx.clone());
+    // }
+    // panel(|cx| {
+    //     text("Panel 2", cx.clone());
+    //     text("B", cx.clone());
+    // }, cx.clone());
 }
 
 #[topo::nested]
-fn panel(children: impl FnOnce(State<Node>), state_parent_node: &State<Node>) {
-    log!("panel");
+fn row(mut cx: Cx, children: impl FnOnce(Cx)) {
+    log!("row");
 
     let state_node = state(|| {
         let el_ws = document().create_element("div").expect("element");
         let node_ws = web_sys::Node::from(el_ws);
-        (*state_parent_node.0).node_ws.append_child(&node_ws);
-        Node { node_ws: web_sys::Node::from(node_ws) }
+        (*cx.state_node.0).node_ws.append_child(&node_ws).expect("append element node");
+        Node { node_ws }
     });
-    children(state_node);
+    cx.state_node = state_node;
+    children(cx);
 }
 
 #[topo::nested]
-fn text(text: &str, state_parent_node: &State<Node>) {  
+fn column(mut cx: Cx, children: impl FnOnce(Cx)) {
+    log!("column");
+
+    let state_node = state(|| {
+        let el_ws = document().create_element("div").expect("element");
+        let node_ws = web_sys::Node::from(el_ws);
+        (*cx.state_node.0).node_ws.append_child(&node_ws).expect("append element node");
+        Node { node_ws }
+    });
+    cx.state_node = state_node;
+    children(cx);
+}
+
+#[topo::nested]
+fn el(mut cx: Cx, children: impl FnOnce(Cx)) {
+    log!("el");
+
+    let state_node = state(|| {
+        let el_ws = document().create_element("div").expect("element");
+        let node_ws = web_sys::Node::from(el_ws);
+        (*cx.state_node.0).node_ws.append_child(&node_ws).expect("append element node");
+        Node { node_ws }
+    });
+    cx.state_node = state_node;
+    children(cx);
+}
+
+#[topo::nested]
+fn text(mut cx: Cx, text: &str) {  
     log!("text");
 
     let state_node = state(|| {
-        let el_ws = document().create_element("span").expect("element");
-        let node_ws = web_sys::Node::from(el_ws);
-
-        let text_node_ws = document().create_text_node(&text).unchecked_into::<web_sys::Node>();
-
-        node_ws.append_child(&text_node_ws);
-
-        (*state_parent_node.0).node_ws.append_child(&node_ws);
+        let node_ws = document().create_text_node(&text).unchecked_into::<web_sys::Node>();
+        (*cx.state_node.0).node_ws.append_child(&node_ws).expect("append text node");
         Node { node_ws }
     });
-    log!("text: {}", text);
-}
-
-struct Node {
-    node_ws: web_sys::Node,
-}
-
-impl Drop for Node {
-    fn drop(&mut self) {
-        let parent = self.node_ws.parent_node().expect("parent Node");
-        parent.remove_child(&self.node_ws);
-        log!("Node dropped");
-    }
+    cx.state_node = state_node;
+    // log!("text cx: {:#?}", cx);
 }
 
