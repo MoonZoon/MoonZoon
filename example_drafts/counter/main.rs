106a83cb794901711e05d41350abca77245a150f
Martin Kavík <martin@kavik.cz>
2020-10-26 08:07:03 +0100

    counter

---
+++ example_drafts/counter/main.rs
@@ -0,0 +1,29 @@
+use zoon::*;
+
+#[Model]
+fn counter() -> ModeL<i32> {
+    use_model(|| 0)
+}
+
+#[Update]
+fn increment() {
+    counter().update(|c| *c += 1);
+}
+
+#[Update]
+fn decrement() {
+    counter().update(|c| *c -= 1);
+}
+
+#[View]
+fn view() -> Row {
+    column![
+        button![text!("-"), button::on_press(decrement)],
+        text!(counter.get()),
+        button![text!("-"), button::on_press(increment)],
+    ]
+}
+
+fn main() {
+    zoon::start("app", view)
+}
