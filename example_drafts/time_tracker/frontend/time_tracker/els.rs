650ee84e2a8b597c1a18c95dfadc0b8419818436
Martin Kavík <martin@kavik.cz>
2020-11-20 22:47:00 +0100

    time_entry_panel

--- example_drafts/time_tracker/frontend/time_tracker/els.rs
+++ example_drafts/time_tracker/frontend/time_tracker/els.rs
@@ -1,5 +1,8 @@
 use zoon::*;
 use crate::app;
+use std::ops::Not;
+
+const DURATION_BREAKPOINT: f64 = 800.;
 
 blocks!{
 
@@ -54,7 +57,7 @@ blocks!{
                 el![project.map(|project| project.name.clone())],
                 start_stop_button(project),
             ],
-            time_entry_panels(client),
+            time_entry_panels(project),
         ]
     }
 
@@ -78,29 +81,73 @@ blocks!{
     // ------ TimeEntry ------
 
     #[el]
-    fn time_entry_panels() -> Column {
-        let clients = super::clients();
+    fn time_entry_panels(project: Var<super::Project>) -> Column {
         column![
-            spacing(30),
-            clients.map(|clients| {
-                clients.map(|clients| clients.iter().rev().map(client_panel))
+            spacing(20),
+            project.map(|project| {
+                project.time_entries.iter().rev().map(time_entry_panel)
             }),
         ]
     }
 
     #[el]
-    fn client_panel(client: Var<super::Client>) -> Column {
-        let statistics = client.map(|client| client.statistics);
+    fn time_entry_panel(time_entry: Var<super::TimeEntry>) -> Column {
+        let show_duration_row = app::viewport_width().inner() < DURATION_BREAKPOINT;
         column![
             row![
-                el![client.map(|client| client.name.clone())],
-                statistics(statistics),
+                time_entry_name(time_entry),
+                button![
+                    button::on_press(|| super::remove_time_entry(time_entry)),
+                    "D",
+                ],
             ],
-            button![
-                button::on_press(|| super::add_time_block(client)),
-                "Add Time Block",
-            ],
-            time_block_panels(client),
+            show_duration_row.then(|| {
+                row![
+                    duration(time_entry)
+                ]
+            }),
+            row![
+                started(),
+                show_duration_row.not().then(|| {
+                    column![
+                        duration(time_entry)
+                    ]
+                }),
+                stopped(),
+            ]
+        ]
+    }
+
+    #[el]
+    fn time_entry_name(time_entry: Var<super::TimeEntry>) -> TextInput {
+        let name = el_var(|| {
+            time_entry.map(|time_entry| time_entry.name.clone())
+        });
+        text_input![
+            text_input::on_change(|new_name| name.set(new_name)),
+            on_blur(|| name.use_ref(|name| {
+                super::rename_time_entry(time_entry, name);
+            })),
+            name.inner(),
+        ]
+    }
+
+    #[el]
+    fn duration(time_entry: Var<super::TimeEntry>) -> TextInput {
+
+    }
+
+    #[el]
+    fn started(time_entry: Var<super::TimeEntry>) -> Column {
+        column![
+
+        ]
+    }
+
+    #[el]
+    fn stopped(time_entry: Var<super::TimeEntry>) -> Column {
+        column![
+            
         ]
     }
     
