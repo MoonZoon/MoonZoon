549dda80c468613388ad6d0706d0fae665cfc35c
Martin Kavík <martin@kavik.cz>
2021-02-23 13:45:07 +0100

    wasm-pack compilation

--- examples/counter/frontend/src/lib.rs
+++ examples/counter/frontend/src/lib.rs
@@ -2,33 +2,33 @@
 
 use zoon::*;
 
-blocks!{
+// blocks!{
 
-    #[s_var]
-    fn counter() -> i32 {
-        0
-    }
+//     #[s_var]
+//     fn counter() -> i32 {
+//         0
+//     }
 
-    #[update]
-    fn increment() {
-        counter().update(|counter| counter + 1);
-    }
+//     #[update]
+//     fn increment() {
+//         counter().update(|counter| counter + 1);
+//     }
 
-    #[update]
-    fn decrement() {
-        counter().update(|counter| counter - 1);
-    }
+//     #[update]
+//     fn decrement() {
+//         counter().update(|counter| counter - 1);
+//     }
 
-    #[el]
-    fn root() -> Column {
-        column![
-            button![button::on_press(decrement), "-"],
-            counter().inner(),
-            button![button::on_press(increment), "+"],
-        ]
-    }
+//     #[el]
+//     fn root() -> Column {
+//         column![
+//             button![button::on_press(decrement), "-"],
+//             counter().inner(),
+//             button![button::on_press(increment), "+"],
+//         ]
+//     }
 
-}
+// }
 
 #[wasm_bindgen(start)]
 pub fn start() {
