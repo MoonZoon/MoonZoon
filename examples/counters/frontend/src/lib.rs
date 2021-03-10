526ad3687539ddf052ef37e658e3bee6dce689b1
Martin Kavík <martin@kavik.cz>
2021-03-10 22:00:51 +0100

    examples: counters, counters_without_macros

---
+++ examples/counters/frontend/src/lib.rs
@@ -0,0 +1,36 @@
+#![no_std]
+
+use zoon::*;
+
+blocks!{
+
+    #[s_var]
+    fn counter() -> i32 {
+        0
+    }
+
+    #[update]
+    fn increment() {
+        counter().update(|counter| counter + 1);
+    }
+
+    #[update]
+    fn decrement() {
+        counter().update(|counter| counter - 1);
+    }
+
+    #[el]
+    fn root() -> Column {
+        column![
+            button![button::on_press(decrement), "-"],
+            counter().inner(),
+            button![button::on_press(increment), "+"],
+        ]
+    }
+
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    start!()
+}
