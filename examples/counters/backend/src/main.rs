482cbaa614ee47abab64601926d9ed4c7b376500
Martin Kavík <martin@kavik.cz>
2021-03-10 22:00:51 +0100

    examples: counters, counters_without_macros

---
+++ examples/counters/backend/src/main.rs
@@ -0,0 +1,13 @@
+use moon::*;
+
+async fn init() {}
+
+async fn frontend() -> Frontend {
+    Frontend::new().title("Counters example")
+}
+
+async fn up_msg_handler(_: UpMsgRequest) {}
+
+fn main() {
+    start!(init, frontend, up_msg_handler).unwrap();
+}
