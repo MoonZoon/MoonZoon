db6d273dc9a31ff52ef7295d7ef9f970c7726bab
Martin Kavík <martin@kavik.cz>
2021-03-12 18:46:04 +0100

    cmp

--- examples/counters/frontend/src/lib.rs
+++ examples/counters/frontend/src/lib.rs
@@ -5,40 +5,75 @@ use zoon::*;
 mod component;
 use component::counter::{self, Counter};
 
-blocks!{
+// blocks!{
 
-    #[s_var]
-    fn counter_count() -> usize {
-        3
-    }
+//     #[s_var]
+//     fn counter_count() -> usize {
+//         3
+//     }
 
-    #[update]
-    fn set_counter_count(count: usize) {
-        counter_count().set(count);
-    }
+//     #[update]
+//     fn set_counter_count(count: usize) {
+//         counter_count().set(count);
+//     }
 
-    #[el]
-    fn root() -> Column {
-        column![
-            main_counter(),
-            counters(),
-        ]
-    }
+//     #[el]
+//     fn root() -> Column {
+//         column![
+//             main_counter(),
+//             counters(),
+//         ]
+//     }
 
-    #[el]
-    fn main_counter() -> Counter {
-        counter![
-            counter_count().inner(),
-            counter::on_change(set_counter_count),
-        ]
-    }
+//     #[el]
+//     fn main_counter() -> Counter {
+//         counter![
+//             counter_count().inner(),
+//             counter::on_change(set_counter_count),
+//         ]
+//     }
 
-    #[el]
-    fn counters() -> Row {
-        row![
-            (0..counter_count().inner()).iter().map(|_| counter![]),
-        ]
-    }
+//     #[el]
+//     fn counters() -> Row {
+//         row![
+//             (0..counter_count().inner()).iter().map(|_| counter![]),
+//         ]
+//     }
+// }
+
+
+fn counter_count() -> usize {
+    3
+}
+
+fn set_counter_count(count: usize) {
+    log!("set_counter_count");
+}
+
+
+#[cmp]
+fn root<'a>() -> Column<'a> {
+    col![
+        main_counter(),
+        counters(),
+    ]
+}
+
+#[cmp]
+fn main_counter() -> Counter {
+    counter![
+        // counter_count().inner(),
+        counter_count(),
+        counter::on_change(set_counter_count),
+    ]
+}
+
+#[cmp]
+fn counters<'a>() -> Row<'a> {
+    row![
+        // (0..counter_count().inner()).iter().map(|_| counter![]),
+        (0..counter_count()).map(|_| counter![]),
+    ]
 }
 
 #[wasm_bindgen(start)]
