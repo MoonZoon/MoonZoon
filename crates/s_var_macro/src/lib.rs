dbfa59a9cda782ed3d77b31286cfbc929f20367b
Martin Kavík <martin@kavik.cz>
2021-03-15 18:29:28 +0100

    s_var

--- crates/s_var_macro/src/lib.rs
+++ crates/s_var_macro/src/lib.rs
@@ -1,4 +1,6 @@
 use proc_macro::TokenStream;
+use quote::ToTokens;
+use syn::{parse_quote, ItemFn, LitStr, ReturnType};
 
 /* 
 #[s_var]
@@ -13,12 +15,31 @@ fn counter_count() -> SVar<i32> {
         3
     })
 }
-
 */
 #[proc_macro_attribute]
 pub fn s_var(_attr: TokenStream, input: TokenStream) -> TokenStream {
+    let mut input_fn: ItemFn = syn::parse(input).unwrap();
 
-    input
+    let input_fn_name = &input_fn.sig.ident;
+    let id = LitStr::new(&input_fn_name.to_string(), input_fn_name.span());
+
+    let inner_block = input_fn.block;
+    input_fn.block = parse_quote!({ 
+        s_var(#id, || {
+            #inner_block
+        })
+    });
+
+    input_fn.sig.output = match input_fn.sig.output {
+        ReturnType::Default => {
+            parse_quote!(-> SVar<()>)
+        },
+        ReturnType::Type(_, type_) => {
+            parse_quote!(-> SVar<#type_>)
+        },
+    };
+
+    input_fn.into_token_stream().into()
 }
 
 #[cfg(test)]
