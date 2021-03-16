4eaf3206e163435e8e0fdc835f764976ea7f9a34
Martin Kavík <martin@kavik.cz>
2021-03-16 22:04:39 +0100

    __BlockCallStack push pop

--- crates/zoon/src/block_call_stack.rs
+++ crates/zoon/src/block_call_stack.rs
@@ -1,12 +1,14 @@
 use crate::runtime::BLOCK_CALL_STACK;
+use crate::log;
 
-pub type Id = i128;
+pub type Id = u128;
 
 #[derive(Default)]
 pub struct __BlockCallStack(Vec<__Block>);
 
 impl __BlockCallStack {
     pub fn push(block: __Block) {
+        log!("Push: {:#?}", block);
         BLOCK_CALL_STACK.with(|call_stack| {
             call_stack
                 .borrow_mut()
@@ -16,6 +18,7 @@ impl __BlockCallStack {
     }
     
     pub fn pop() -> Option<__Block> {
+        log!("Pop");
         BLOCK_CALL_STACK.with(|call_stack| {
             call_stack
                 .borrow_mut()
@@ -25,6 +28,7 @@ impl __BlockCallStack {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 pub enum __Block {
     SVar(Id),
     Cache(Id),
