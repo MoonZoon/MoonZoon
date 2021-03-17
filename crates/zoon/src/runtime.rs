e8f33094c831246a968927e8bca517970f7de314
Martin Kavík <martin@kavik.cz>
2021-03-17 14:37:50 +0100

    register dependencies

--- crates/zoon/src/runtime.rs
+++ crates/zoon/src/runtime.rs
@@ -2,6 +2,7 @@ use crate::l_var_map::LVarMap;
 use crate::s_var_map::SVarMap;
 use crate::cache_map::CacheMap;
 use crate::block_call_stack::__BlockCallStack;
+use crate::relations::__Relations;
 use crate::root;
 use crate::element::Element;
 use std::cell::RefCell;
@@ -13,6 +14,7 @@ thread_local! {
     pub(crate) static LVARS: RefCell<LVarMap> = RefCell::new(LVarMap::new());
     pub(crate) static ROOT_CMP: RefCell<Option<Box<dyn Fn() -> Box<dyn Element>>>> = RefCell::new(None);
     pub(crate) static BLOCK_CALL_STACK: RefCell<__BlockCallStack> = RefCell::new(__BlockCallStack::default());
+    pub(crate) static RELATIONS: RefCell<__Relations> = RefCell::new(__Relations::default());
 }
 
 pub fn rerender() {
