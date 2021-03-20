95caa5beb0715ce47eb5dc6d617edff904e66f5f
Martin Kavík <martin@kavik.cz>
2021-03-20 22:19:48 +0100

    component.rs

--- crates/cmp_macro/src/lib.rs
+++ crates/cmp_macro/src/lib.rs
@@ -1,11 +1,9 @@
 use proc_macro::TokenStream;
-use quote::ToTokens;
-use syn::{parse_quote, ItemFn, ReturnType, Type, spanned::Spanned};
-use uuid::Uuid;
+use syn::{parse_quote, ItemFn, spanned::Spanned};
 
 /* 
 #[cmp]
-fn root<'a>() -> Column<'a> {
+fn root<'a>() -> Cmp<'a> {
     col![
         control_counters(),
         counters(),
@@ -14,91 +12,28 @@ fn root<'a>() -> Column<'a> {
 
 // to 
 
-fn root<'a>() -> Column<'a> {
-    col![
-        control_counters(),
-        counters(),
-    ]
-}
-
-fn counter_count() -> Cache<i32> {
-    const __ID: u128 = 0x936DA01F9ABD4D9D80C702AF85C822A8;
-    const __BLOCK: __Block = __Block::Cache(__ID);
-    __Relations::add_dependency(__BLOCK);
-    cache(__ID, || {
-        __BlockCallStack::push(__BLOCK);
-        __Relations::remove_dependencies(__BLOCK);
-        let output = (|| {
-            3
-        })();
-        __BlockCallStack::pop();
-        output
-    })
+fn root<'a>() -> Cmp<'a> {
+    let creator = l_var(move || (move || {
+        col![
+            control_counters(),
+            counters(),
+        ]
+    }));
+    creator.inner()().into_component()
 }
 */
 #[proc_macro_attribute]
 pub fn cmp(_args: TokenStream, input: TokenStream) -> TokenStream {
     let mut input_fn: ItemFn = syn::parse(input).unwrap();
 
+    let inner_block = input_fn.block;
+    input_fn.block = parse_quote!({ 
+        let creator = l_var(move || (move || #inner_block));
+        creator.inner()().into_component()
+    });
+
     quote::quote_spanned!(input_fn.span()=>
         #[tracked_call]
         #input_fn
     ).into()
-
-
-
-
-
-    // let mut input_fn: ItemFn = syn::parse(input).unwrap();
-
-    // let id = Uuid::new_v4().as_u128();
-
-    // let inner_block = input_fn.block;
-    // input_fn.block = parse_quote!({ 
-    //     const __ID: u128 = #id;
-    //     const __BLOCK: __Block = __Block::Cache(__ID);
-    //     __Relations::add_dependency(__BLOCK);
-    //     cache(__ID, || {
-    //         __BlockCallStack::push(__BLOCK);
-    //         __Relations::remove_dependencies(&__BLOCK);
-    //         let output = (|| #inner_block)();
-    //         __BlockCallStack::pop();
-    //         output
-    //     })
-    // });
-
-    // input_fn.sig.output = match input_fn.sig.output {
-    //     ReturnType::Default => {
-    //         parse_quote!(-> Cache<()>)
-    //     },
-    //     ReturnType::Type(_, type_) if is_first_segment_cache(&type_)  => {
-    //         parse_quote!(-> #type_)
-    //     },
-    //     ReturnType::Type(_, type_) => {
-    //         parse_quote!(-> Cache<#type_>)
-    //     },
-    // };
-
-    // input_fn.into_token_stream().into()
-}
-
-fn is_first_segment_cache(type_: &Box<Type>) -> bool {
-    if let Type::Path(type_path) = type_.as_ref() {
-        type_path
-            .path
-            .segments
-            .first()
-            .map(|path_segment| path_segment.ident == "Cache")
-            .unwrap_or_default()
-    } else {
-        false
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
