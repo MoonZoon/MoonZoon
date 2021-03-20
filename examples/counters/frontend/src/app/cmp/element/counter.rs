7777da0b6cac9f92839512a5416a1f88be558b69
Martin Kavík <martin@kavik.cz>
2021-03-20 19:45:11 +0100

    cmp - tracked_call

--- examples/counters/frontend/src/app/cmp/element/counter.rs
+++ examples/counters/frontend/src/app/cmp/element/counter.rs
@@ -18,7 +18,7 @@ pub struct Counter {
 impl Element for Counter {
     #[render]
     fn render(&mut self, rcx: RenderContext) {
-        // log!("Counter ID: {:#?}", __TrackedCallId::current());
+        // log!("Counter ID: {:#?}", TrackedCallId::current());
 
         let on_change = self.on_change.take().map(|on_change| on_change.0);
         let step = self.step.unwrap_or(1);
