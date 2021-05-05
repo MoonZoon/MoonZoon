8b14735842ed5b96968368f5f0865637ce58a824
Martin Kavík <martin@kavik.cz>
2021-05-05 12:23:21 +0200

    counter, alternative, counters + keyed updated, features

--- crates/zoon/src/lib.rs
+++ crates/zoon/src/lib.rs
@@ -1,5 +1,4 @@
 pub use wasm_bindgen::{self, prelude::*, JsCast};
-pub use static_ref_macro::static_ref;
 
 pub mod element;
 mod dom;
@@ -9,8 +8,6 @@ mod futures_signals_ext;
 pub use futures_signals_ext::{MutableExt, MutableVecExt};
 pub use element::*;
 pub use dom::{window, document}; 
-pub use console::log;
-pub use once_cell;
 pub use futures_signals::{
     self,
     map_mut,
@@ -20,15 +17,38 @@ pub use futures_signals::{
     signal_map::{MutableBTreeMap, SignalMap, SignalMapExt},
 };
 pub use dominator::{self, Dom, DomBuilder, events, traits::StaticEvent};
-pub use enclose::enc as clone;
 pub use paste;
+pub use console::log;
+
+#[cfg(feature = "static_ref")]
+pub use static_ref_macro::static_ref;
+#[cfg(feature = "static_ref")]
+pub use once_cell;
+
+#[cfg(feature = "clone")]
+pub use enclose::enc as clone;
+
+#[cfg(feature = "alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[cfg(feature = "fmt")]
 pub use ufmt::{self, uDebug, uDisplay, uWrite, uwrite, uwriteln};
+#[cfg(feature = "fmt")]
 pub use lexical::{self, WriteIntegerOptions, WriteFloatOptions, NumberFormat};
 
-use wee_alloc::WeeAlloc;
+#[cfg(not(feature = "fmt"))]
+pub use std::format as format;
 
-#[global_allocator]
-static ALLOC: WeeAlloc = WeeAlloc::INIT;
+#[cfg(feature = "fmt")]
+#[macro_export]
+macro_rules! format {
+    ($($arg:tt)*) => {{
+        let mut text = String::new();
+        $crate::ufmt::uwrite!(&mut text, $($arg)*).unwrap_throw();
+        text
+    }}
+}
 
 pub trait FlagSet {}
 pub trait FlagNotSet {}
@@ -47,16 +67,8 @@ macro_rules! make_flags {
     }
 }
 
-#[macro_export]
-macro_rules! format {
-    ($($arg:tt)*) => {{
-        let mut text = String::new();
-        $crate::ufmt::uwrite!(&mut text, $($arg)*).unwrap_throw();
-        text
-    }}
-}
-
 pub fn start_app<'a, E: Element>(browser_element_id: impl Into<Option<&'a str>>, view_root: impl FnOnce() -> E) {
+    #[cfg(feature = "panic_hook")]
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
--- examples/counter/frontend/src/lib.rs
+++ examples/counter/frontend/src/lib.rs
@@ -15,12 +15,26 @@ fn decrement() {
 
 fn root() -> impl Element {
     Column::new()
-        .item(Button::new().on_press(decrement).label("-"))
+        .item(Button::new().label("-").on_press(decrement))
         .item(Text::with_signal(counter().signal()))
-        .item(Button::new().on_press(increment).label("+"))
+        .item(Button::new().label("+").on_press(increment))
 }
+
+// ------ Alternative ------
+
+fn _root() -> impl Element {
+    let counter = std::rc::Rc::new(Mutable::new(0));
+    let on_press = clone!((counter) move |step: i32| *counter.lock_mut() += step);
+    Column::new()
+        .item(Button::new().label("-").on_press(clone!((on_press) move || on_press(-1))))
+        .item_signal(counter.signal())
+        .item(Button::new().label("+").on_press(move || on_press(1)))
+}
+
+// ---------- // ----------- 
 
 #[wasm_bindgen(start)]
 pub fn start() {
     start_app("app", root);
 }
+
