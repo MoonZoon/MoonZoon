718ebdbbdc45656d8e83993230d607f7aed6b959
Martin Kavík <martin@kavik.cz>
2021-06-03 12:01:13 +0200

    remove async_compression

--- crates/mzoon/src/file_compressor.rs
+++ crates/mzoon/src/file_compressor.rs
@@ -1,14 +1,10 @@
-use async_compression::{Level, futures::write::{GzipEncoder, BrotliEncoder}};
-use futures::{AsyncWrite, AsyncWriteExt};
-use tokio_util::compat::{TokioAsyncWriteCompatExt, Compat};
-use tokio::{fs, task::JoinHandle, spawn};
+use tokio::{fs, task::JoinHandle, spawn, io::AsyncWriteExt};
 use anyhow::{Context, Result};
-use std::{sync::Arc, path::Path};
+use std::{sync::Arc, path::Path, io::Read};
+use brotli::{CompressorReader as BrotliEncoder, enc::backward_references::BrotliEncoderParams};
+use flate2::{bufread::GzEncoder, Compression as GzCompression};
 
-pub type BrotliFileCompressor = BrotliEncoder<Compat<fs::File>>;
-pub type GzipFileCompressor = GzipEncoder<Compat<fs::File>>;
-
-pub trait FileCompressor where Self: AsyncWrite + Unpin + Send + Sized {
+pub trait FileCompressor {
     fn compress_file(
         content: Arc<Vec<u8>>, 
         path: &Path, 
@@ -20,28 +16,41 @@ pub trait FileCompressor where Self: AsyncWrite + Unpin + Send + Sized {
         let path = path.with_extension(file_extension);
 
         spawn(async move {
-            let file_writer = fs::File::create(&path)
+            let mut file_writer = fs::File::create(&path)
                 .await
                 .with_context(|| format!("Failed to create the file {:#?}", path))?;
 
-            let mut compressor = Self::with_quality(file_writer.compat_write(), Level::Best);
-            compressor.write_all(&content).await?;
-            compressor.flush().await?;
+            let compressed_content = Self::compress(&content)?;
+            file_writer.write_all(&compressed_content).await?;
+            file_writer.flush().await?;
             Ok(())
         })
     }
 
-    fn with_quality(inner: Compat<fs::File>, level: Level) -> Self;
+    fn compress(bytes: &[u8]) -> Result<Vec<u8>>;
 }
 
+pub struct BrotliFileCompressor;
+
 impl FileCompressor for BrotliFileCompressor {
-    fn with_quality(inner: Compat<fs::File>, level: Level) -> Self {
-        Self::with_quality(inner, level)
+    fn compress(bytes: &[u8]) -> Result<Vec<u8>> {
+        let params = BrotliEncoderParams::default();
+        let mut compressor = BrotliEncoder::with_params(bytes, 0, &params);
+
+        let mut compressed_bytes = Vec::new();
+        compressor.read_to_end(&mut compressed_bytes)?;
+        Ok(compressed_bytes)
     }
 }
 
+pub struct GzipFileCompressor;
+
 impl FileCompressor for GzipFileCompressor {
-    fn with_quality(inner: Compat<fs::File>, level: Level) -> Self {
-        Self::with_quality(inner, level)
+    fn compress(bytes: &[u8]) -> Result<Vec<u8>> {
+        let mut compressor = GzEncoder::new(bytes, GzCompression::best());
+
+        let mut compressed_bytes = Vec::new();
+        compressor.read_to_end(&mut compressed_bytes)?;
+        Ok(compressed_bytes)
     }
 }
