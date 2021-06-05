5c349ba3e665771f69053e979124f3c4461d60ba
Martin Kavík <martin@kavik.cz>
2021-06-05 14:57:25 +0200

    frontend.rs refactor

--- crates/mzoon/src/frontend.rs
+++ crates/mzoon/src/frontend.rs
@@ -1,11 +1,13 @@
 use tokio::fs;
-use tokio::{try_join, join};
-use std::path::{Path, PathBuf};
+use tokio::try_join;
+use std::path::Path;
 use uuid::Uuid;
 use anyhow::{Context, Error};
 use std::sync::Arc;
 use futures::TryStreamExt;
 use fehler::throws;
+use const_format::concatcp;
+use std::borrow::Cow;
 use crate::wasm_pack::{check_or_install_wasm_pack, build_with_wasm_pack};
 use crate::helper::{BrotliFileCompressor, GzipFileCompressor, FileCompressor, visit_files, AsyncReadToVec};
 
@@ -14,48 +16,18 @@ use crate::helper::{BrotliFileCompressor, GzipFileCompressor, FileCompressor, vi
 #[throws]
 pub async fn build_frontend(release: bool, cache_busting: bool) {
     println!("Building frontend...");
-
     check_or_install_wasm_pack()?;
-
-    let old_build_id = fs::read_to_string("frontend/pkg/build_id")
-        .await
-        .ok()
-        .map(|uuid| uuid.parse::<u128>().map(|uuid| uuid).unwrap_or_default());
-
-    if let Some(old_build_id) = old_build_id {
-        let old_wasm = format!("frontend/pkg/frontend_bg_{}.wasm", old_build_id);
-        let old_js = format!("frontend/pkg/frontend_{}.js", old_build_id);
-        let _ = join!(
-            fs::remove_file(&old_wasm),
-            fs::remove_file(&old_js),
-            fs::remove_file(format!("{}.br", &old_wasm)),
-            fs::remove_file(format!("{}.br", &old_js)),
-            fs::remove_file(format!("{}.gz", &old_wasm)),
-            fs::remove_file(format!("{}.gz", &old_js)),
-            fs::remove_dir_all("frontend/pkg/snippets"),
-        );
-    }
-
+    remove_pkg().await?;
     build_with_wasm_pack(release)?;
 
-    let build_id = cache_busting
-        .then(|| Uuid::new_v4().as_u128())
-        .unwrap_or_default();
-
-    let wasm_file_path = Path::new("frontend/pkg/frontend_bg.wasm");
-    let new_wasm_file_path =
-        PathBuf::from(format!("frontend/pkg/frontend_bg_{}.wasm", build_id));
-    let js_file_path = Path::new("frontend/pkg/frontend.js");
-    let new_js_file_path = PathBuf::from(format!("frontend/pkg/frontend_{}.js", build_id));
-
-    try_join!(
-        async { fs::rename(wasm_file_path, &new_wasm_file_path).await.context("Failed to rename the Wasm file in the pkg directory") },
-        async { fs::rename(js_file_path, &new_js_file_path).await.context("Failed to rename the JS file in the pkg directory") },
-        async { fs::write("frontend/pkg/build_id", build_id.to_string()).await.context("Failed to write the frontend build id") },
+    let build_id = Uuid::new_v4().as_u128();
+    let (wasm_file_path, js_file_path, _) = try_join!(
+        rename_wasm_file(build_id, cache_busting),
+        rename_js_file(build_id, cache_busting),
+        write_build_id(build_id),
     )?;
-
     if release {
-        compress_pkg(&new_wasm_file_path, &new_js_file_path).await?;
+        compress_pkg(wasm_file_path.as_ref(), js_file_path.as_ref()).await?;
     }
     println!("Frontend built");
 }
@@ -63,7 +35,54 @@ pub async fn build_frontend(release: bool, cache_busting: bool) {
 // -- private --
 
 #[throws]
-async fn compress_pkg(wasm_file_path: &Path, js_file_path: &Path) {
+async fn remove_pkg() {
+    let pkg_path = Path::new("frontend/pkg");
+    if pkg_path.is_dir() {
+        fs::remove_dir_all(pkg_path).await.context("Failed to remove pkg")?;
+    }
+}
+
+#[throws]
+async fn write_build_id(build_id: u128) {
+    fs::write("frontend/pkg/build_id", build_id.to_string())
+        .await
+        .context("Failed to write the frontend build id")?;
+}
+
+#[throws]
+async fn rename_wasm_file(build_id: u128, cache_busting: bool) -> Cow<'static, str> {
+    const PATH: &str = "frontend/pkg/frontend_bg";
+    const EXTENSION: &str = ".wasm";
+    const ORIGINAL_PATH: &str = concatcp!(PATH, EXTENSION);
+
+    if !cache_busting { return Cow::from(ORIGINAL_PATH) };
+
+    let new_path = format!("{}_{}{}", PATH, build_id, EXTENSION);
+    fs::rename(ORIGINAL_PATH, &new_path)
+        .await
+        .context("Failed to rename the wasm file in the pkg directory")?;
+
+    Cow::from(new_path)
+}
+
+#[throws]
+async fn rename_js_file(build_id: u128, cache_busting: bool) -> Cow<'static, str> {
+    const PATH: &str = "frontend/pkg/frontend";
+    const EXTENSION: &str = ".js";
+    const ORIGINAL_PATH: &str = concatcp!(PATH, EXTENSION);
+
+    if !cache_busting { return Cow::from(ORIGINAL_PATH) };
+
+    let new_path = format!("{}_{}{}", PATH, build_id, EXTENSION);
+    fs::rename(ORIGINAL_PATH, &new_path)
+        .await
+        .context("Failed to rename the JS file in the pkg directory")?;
+
+    Cow::from(new_path)
+}
+
+#[throws]
+async fn compress_pkg(wasm_file_path: impl AsRef<Path>, js_file_path: impl AsRef<Path>) {
     try_join!(
         create_compressed_files(wasm_file_path),
         create_compressed_files(js_file_path),
