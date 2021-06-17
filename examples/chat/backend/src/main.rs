02f32ecaaae80befabe8af7429547fc026a0f8cb
Martin Kavík <martin@kavik.cz>
2021-06-17 18:04:59 +0200

    chat from counter, chat_old

--- examples/chat/backend/src/main.rs
+++ examples/chat/backend/src/main.rs
@@ -1,20 +1,41 @@
 use moon::*;
-use shared::{UpMsg, DownMsg, Message};
-
-async fn init() {}
 
 async fn frontend() -> Frontend {
-    Frontend::new().title("Chat example")
+    Frontend::new()
+        .title("Chat example")
+        .append_to_head("<style>html {
+            background-color: black;
+            color: lightgray;
+        }
+        
+        #app * {
+            padding: 5px;
+        }
+            
+        .button {
+            cursor: pointer;
+            background-color: darkgreen;
+            user-select: none;
+        }
+        
+        .button:hover {
+            background-color: green;
+        }
+        
+        .column {
+            display: flex;
+            flex-direction: column;
+        }
+        
+        .row {
+            display: flex;
+        }
+        </style>")
 }
 
-async fn up_msg_handler(req: UpMsgRequest) {
-    if let UpMsg::SendMessage(message) = req.up_msg {
-        join_all(connected_client::by_id().iter().map(|(_, client)| {
-            client.send_down_msg(message, req.cor_id)
-        })).await
-    }
-}
+async fn up_msg_handler(_: UpMsgRequest) {}
 
-fn main() {
-    start!(init, frontend, up_msg_handler);
+#[moon::main]
+async fn main() -> std::io::Result<()> {
+    start(frontend, up_msg_handler, |_|{}).await
 }
--- examples/chat/frontend/src/lib.rs
+++ examples/chat/frontend/src/lib.rs
@@ -1,136 +1,39 @@
-#![no_std]
-
 use zoon::*;
-use shared::{UpMsg, DownMsg, Message};
-use std::mem;
 
-blocks!{
-
-    #[s_var]
-    fn username() -> String {
-        "John".to_owned()
-    }
-
-    #[update]
-    fn set_username(username: String) {
-        username().set(username);
-    }
-
-    #[s_var]
-    fn messages() -> Vec<VarC<Message>> {
-        Vec::new()
-    }
-
-    #[s_var]
-    fn new_message_text() -> String {
-        String::new()
-    }
-
-    #[update]
-    fn set_new_message_text(text: String) {
-        new_message_text().set(text);
-    }
-
-    #[s_var]
-    fn connection() -> Connection<UpMsg, DownMsg> {
-        Connection::new(|down_msg, _| {
-            if let DownMsg::MessageReceived(message) = down_msg {
-                messages().update_mut(|messages| messages.push(new_var_c(message)));
-            }
-        })
-    }
-
-    #[update]
-    fn send_message() {
-        connection().use_ref(|connection| {
-            connection.send_up_msg(UpMsg::SendMessage(Message {
-                username: username().inner(),
-                text: new_message_text().map_mut(mem::take),
-            }), None);
-        });
-    }
-
-    #[el]
-    fn root() -> Column {
-        column![
-            received_messages(),
-            new_message_panel(),
-            username_panel(),
-        ]
-    }
-
-    #[el]
-    fn received_messages() -> Column [
-        let messages = messages().map(|messages| {
-            messages.iter_vars().map(received_message)
-        });
-        column![
-            messages
-        ]
-    ]
-
-    #[el]
-    fn received_message(message: Var<Message>) -> Row {
-        let message = message.inner();
-        row![
-            column![
-                el![
-                    font::bold(),
-                    message.username,
-                ],
-                message.text
-            ]
-        ]
-    }
-
-    #[el]
-    fn new_message_panel() -> Row {
-        let new_message_text = new_message_text().inner();
-        row![
-            text_input![
-                do_once(focus),
-                text_input::on_change(set_new_message_text),
-                input::label_hidden("New message text"),
-                placeholder![
-                    placeholder::text("Message"),
-                ],
-                on_key_down(|event| {
-                    if let Key::Enter = event.key {
-                        send_message()
-                    }
-                }),
-                new_message_text,
-            ],
-            button![
-                button::on_press(send_message), 
-                "Send",
-            ],
-        ]
-    }
-
-    #[el]
-    fn username_panel() -> Row {
-        let input_id = el_var(ElementId::new);
-        let username = username().inner();
-        row![
-            label![
-                label::for_input(input_id.inner()),
-                "Username:",
-            ],
-            text_input![
-                id(input_id.inner()),
-                text_input::on_change(set_username),
-                placeholder![
-                    placeholder::text("Joe"),
-                ],
-                username,
-            ],
-        ]
-    }
-
+#[static_ref]
+fn counter() -> &'static Mutable<i32> {
+    Mutable::new(0)
 }
+
+fn increment() {
+    counter().update(|counter| counter + 1)
+}
+
+fn decrement() {
+    counter().update(|counter| counter - 1)
+}
+
+fn root() -> impl Element {
+    Column::new()
+        .item(Button::new().label("-").on_press(decrement))
+        .item(Text::with_signal(counter().signal()))
+        .item(Button::new().label("+").on_press(increment))
+}
+// ------ Alternative ------
+
+fn _root() -> impl Element {
+    let counter = Mutable::new(0);
+    let on_press = clone!((counter) move |step: i32| *counter.lock_mut() += step);
+    Column::new()
+        .item(Button::new().label("-").on_press(clone!((on_press) move || on_press(-1))))
+        .item_signal(counter.signal())
+        .item(Button::new().label("+").on_press(move || on_press(1)))
+}
+
+// ---------- // ----------- 
 
 #[wasm_bindgen(start)]
 pub fn start() {
-    start!()
+    start_app("app", root);
 }
+
