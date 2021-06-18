ed2895a67dec8a5ab5bed19eb64ed0827f47cc64
Martin Kavík <martin@kavik.cz>
2021-06-18 14:09:55 +0200

    fixe repository url, remove chat_old, wip chat

--- examples/chat/frontend/src/lib.rs
+++ examples/chat/frontend/src/lib.rs
@@ -1,39 +1,120 @@
 use zoon::*;
+use std::mem;
+use shared::{UpMsg, DownMsg, Message};
 
 #[static_ref]
-fn counter() -> &'static Mutable<i32> {
-    Mutable::new(0)
+fn username() -> &'static Mutable<String> {
+    Mutable::new("John".to_owned())
 }
 
-fn increment() {
-    counter().update(|counter| counter + 1)
+fn set_username(name: String) {
+    username().set(name);
 }
 
-fn decrement() {
-    counter().update(|counter| counter - 1)
+#[static_ref]
+fn messages() -> &'static MutableVec<Message> {
+    MutableVec::new()
+}
+
+#[static_ref]
+fn new_message_text() -> &'static Mutable<String> {
+    Mutable::new(String::new())
+}
+
+fn set_new_message_text(text: String) {
+    new_message_text().set(text);
+}
+
+// #[static_ref]
+// fn connection() -> &'static Connection<UpMsg, DownMsg> {
+//     Connection::new(|down_msg, _| {
+//         if let DownMsg::MessageReceived(message) = down_msg {
+//             messages().lock_mut().push_cloned(message);
+//         }
+//     })
+// }
+
+fn send_message() {
+    // connection.send_up_msg(UpMsg::SendMessage(Message {
+    //     username: username().get_cloned(),
+    //     text: new_message_text().map_mut(mem::take),
+    // }), None);
 }
 
 fn root() -> impl Element {
     Column::new()
-        .item(Button::new().label("-").on_press(decrement))
-        .item(Text::with_signal(counter().signal()))
-        .item(Button::new().label("+").on_press(increment))
-}
-// ------ Alternative ------
-
-fn _root() -> impl Element {
-    let counter = Mutable::new(0);
-    let on_press = clone!((counter) move |step: i32| *counter.lock_mut() += step);
-    Column::new()
-        .item(Button::new().label("-").on_press(clone!((on_press) move || on_press(-1))))
-        .item_signal(counter.signal())
-        .item(Button::new().label("+").on_press(move || on_press(1)))
+        .item(received_messages())
+        .item(new_message_panel())
+        .item(username_panel())
 }
 
-// ---------- // ----------- 
+fn received_messages() -> impl Element {
+    Column::new().items_signal_vec(
+        // messages().signal_vec_cloned().map(received_message)
+        messages().signal_vec_cloned().map(received_message)
+    )
+}
+
+fn received_message(message: Message) -> impl Element {
+    Row::new()
+        .item(Column::new()
+            .item(El::new()
+                // .style(Font::new().bold())
+                .child(message.username)
+            )
+            .item(message.text)
+        )
+}
+
+fn new_message_panel() -> impl Element {
+    Row::new()
+        .item(new_message_input())
+        .item(send_button())
+}
+
+fn new_message_input() -> impl Element {
+    Text::new("there will be TextInput")
+    // TextInput::new()
+        // .focus()
+        // .on_change(set_new_message_text)
+        // .label_hidden("New message text")
+        // .placeholder(Placeholder::new("Message"))
+        // .on_key_down(|event| {
+        //     if let Key::Enter = event.key {
+        //         send_message()
+        //     }
+        // })
+        // .text_signal(new_message_text().signal_cloned())
+}
+
+fn send_button() -> impl Element {
+    Button::new()
+        .on_press(send_message)
+        .label("Send")
+}
+
+fn username_panel() -> impl Element {
+    let id = "username_input";
+    Row::new()
+        .item(username_input_label(id))
+        .item(username_input(id))
+}
+
+fn username_input_label(id: &str) -> impl Element {
+    Label::new()
+        .for_input(id)
+        .label("Username:")
+}
+
+fn username_input(id: &str) -> impl Element {
+    TextInput::new()
+        .id(id)
+        .on_change(set_username)
+        .placeholder(Placeholder::new("Joe"))
+        .text_signal(username().signal_cloned())
+}
 
 #[wasm_bindgen(start)]
 pub fn start() {
     start_app("app", root);
 }
-
--- examples/chat/shared/src/lib.rs
+++ examples/chat/shared/src/lib.rs
@@ -1,1 +1,17 @@
+use serde_lite::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Message {
+    pub username: String,
+    pub text: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum UpMsg {
+    SendMessage(Message),
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum DownMsg {
+    MessageReceived(Message)
+}
