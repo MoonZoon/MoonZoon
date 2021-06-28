2391af24e4bd3a6a2823febd2d8bc9fb98155dac
Martin Kavík <martin@kavik.cz>
2021-06-28 22:09:32 +0200

    style -> s

--- crates/zoon/src/element/ability.rs
+++ crates/zoon/src/element/ability.rs
@@ -12,7 +12,7 @@ pub trait Focusable: UpdateRawEl<RawHtmlEl> + Sized {
 // ------ Styleable ------
 
 pub trait Styleable<'a, T: RawEl>: UpdateRawEl<T> + Sized {
-    fn style(self, style: impl Style<'a>) -> Self {
+    fn s(self, style: impl Style<'a>) -> Self {
         self.update_raw_el(|raw_el| {
             style.update_raw_el_style(raw_el)
         })
