a609066be7ca5ca28bf6bebbc4014872de132488
Martin Kavík <martin@kavik.cz>
2021-06-29 18:41:39 +0200

    example svg updated

--- examples/svg/backend/src/main.rs
+++ examples/svg/backend/src/main.rs
@@ -4,9 +4,9 @@ async fn frontend() -> Frontend {
     Frontend::new().title("SVG example")
 }
 
-async fn up_msg_handler(_: UpMsgRequest) {}
+async fn up_msg_handler(_: UpMsgRequest<()>) {}
 
 #[moon::main]
 async fn main() -> std::io::Result<()> {
-    start(frontend, up_msg_handler, |_|{}).await
+    start(frontend, up_msg_handler, |_| {}).await
 }
