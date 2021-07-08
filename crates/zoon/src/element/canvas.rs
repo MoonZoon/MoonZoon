499505b57622976f8a34e56abaeab15d5d2dff37
Martin Kavík <martin@kavik.cz>
2021-07-08 21:45:52 +0200

    Canvas

---
+++ crates/zoon/src/element/canvas.rs
@@ -0,0 +1,74 @@
+use crate::*;
+use std::marker::PhantomData;
+
+// ------ ------
+//    Element
+// ------ ------
+
+make_flags!(Width, Height);
+
+pub struct Canvas<WidthFlag, HeightFlag> {
+    raw_el: RawHtmlEl,
+    flags: PhantomData<(WidthFlag, HeightFlag)>,
+}
+
+impl Canvas<WidthFlagNotSet, HeightFlagNotSet> {
+    pub fn new() -> Self {
+        Self {
+            raw_el: RawHtmlEl::new("canvas").attr("class", "canvas"),
+            flags: PhantomData,
+        }
+    }
+}
+
+impl<HeightFlag> Element for Canvas<WidthFlagSet, HeightFlag> {
+    fn into_raw_element(self) -> RawElement {
+        self.raw_el.into()
+    }
+}
+
+impl<WidthFlag, HeightFlag> UpdateRawEl<RawHtmlEl> for Canvas<WidthFlag, HeightFlag> {
+    fn update_raw_el(mut self, updater: impl FnOnce(RawHtmlEl) -> RawHtmlEl) -> Self {
+        self.raw_el = updater(self.raw_el);
+        self
+    }
+}
+
+// ------ ------
+//   Abilities
+// ------ ------
+
+impl<WidthFlag, HeightFlag> Styleable<'_, RawHtmlEl> for Canvas<WidthFlag, HeightFlag> {}
+impl<WidthFlag, HeightFlag> KeyboardEventAware<RawHtmlEl> for Canvas<WidthFlag, HeightFlag> {}
+impl<WidthFlag, HeightFlag> Hoverable<RawHtmlEl> for Canvas<WidthFlag, HeightFlag> {}
+
+// ------ ------
+//  Attributes
+// ------ ------
+
+impl<'a, WidthFlag, HeightFlag> Canvas<WidthFlag, HeightFlag> {
+    /// Default: 300px
+    pub fn width(mut self, width: u32) -> Canvas<WidthFlagSet, HeightFlag>
+    where
+        WidthFlag: FlagNotSet,
+    {
+        self.raw_el = self.raw_el.attr("width", &width.to_string());
+        self.into_type()
+    }
+
+    /// Default: 150px
+    pub fn height(mut self, height: u32) -> Canvas<WidthFlag, HeightFlagSet>
+    where
+        HeightFlag: FlagNotSet,
+    {
+        self.raw_el = self.raw_el.attr("height", &height.to_string());
+        self.into_type()
+    }
+
+    fn into_type<NewWidthFlag, NewHeightFlag>(self) -> Canvas<NewWidthFlag, NewHeightFlag> {
+        Canvas {
+            raw_el: self.raw_el,
+            flags: PhantomData,
+        }
+    }
+}
