5e70e003d0b868eccf7db86d9fe940f16f18ecfd
Martin Kavík <martin@kavik.cz>
2021-07-08 20:25:40 +0200

    canvas example - skeleton + change_color_button

--- examples/canvas/frontend/src/lib.rs
+++ examples/canvas/frontend/src/lib.rs
@@ -1,108 +1,97 @@
 use zoon::*;
 
+// ------ ------
+//    Statics
+// ------ ------
+
+#[derive(Copy, Clone)]
+enum Color {
+    Red,
+    Blue,
+}
+
 #[static_ref]
-fn counter() -> &'static Mutable<i32> {
-    Mutable::new(0)
+fn color() -> &'static Mutable<Color> {
+    Mutable::new(Color::Red)
 }
 
-fn increment() {
-    counter().update(|counter| counter + 1)
+// ------ ------
+//    Signals
+// ------ ------
+
+fn fill_style() -> impl Signal<Item = JsValue> {
+    color().signal().map(|color| {
+        match color {
+            Color::Red => "red",
+            Color::Blue => "blue"
+        }.into()
+    })
 }
 
-fn decrement() {
-    counter().update(|counter| counter - 1)
+// ------ ------
+//   Commands
+// ------ ------
+
+fn toggle_color() {
+    use self::Color::{Red, Blue};
+    color().update(|color| if let Red = color { Blue } else { Red });
 }
+
+// ------ ------
+//     View
+// ------ ------
 
 fn root() -> impl Element {
-    Column::new()
-        .item(Button::new().label("-").on_press(decrement))
-        .item(Text::with_signal(counter().signal()))
-        .item(Button::new().label("+").on_press(increment))
+    Row::new()
+        .s(Spacing::new(20))
+        .item(canvas())
+        .item(change_color_button())
 }
 
-// ------ Alternative ------
-fn _root() -> impl Element {
-    let (counter, counter_signal) = Mutable::new_and_signal(0);
-    let on_press = move |step: i32| *counter.lock_mut() += step;
-    Column::new()
-        .item(
-            Button::new()
-                .label("-")
-                .on_press(clone!((on_press) move || on_press(-1))),
-        )
-        .item_signal(counter_signal)
-        .item(Button::new().label("+").on_press(move || on_press(1)))
+fn canvas() -> impl Element {
+    Text::new("canvas")
+
+    // let fill_style = fill_style(); 
+    // canvas![
+    //         canvas::width(300),
+    //         canvas::height(300),
+    //         canvas::on_ready(|canvas| {
+    //             let ctx = canvas.context_2d();
+    //             ctx.lineWidth = 10;
+    //             fill_style.use_ref(|style| ctx.set_fill_style(style));
+    //             // Wall
+    //             ctx.stroke_rect(75., 140., 150., 110.);
+    //             // Door
+    //             ctx.fill_rect(130., 190., 40., 60.);
+    //             // Roof
+    //             ctx.begin_path();
+    //             ctx.move_to(50., 140.);
+    //             ctx.line_to(150., 60.);
+    //             ctx.line_to(250., 140.);
+    //             ctx.close_path();
+    //             ctx.stroke();
+    //         });
+    //     ],
 }
-// ---------- // -----------
+
+fn change_color_button() -> impl Element {
+    let (hovered, hovered_signal) = Mutable::new_and_signal(false);
+    Button::new()
+        .s(Padding::new().all(6))
+        .s(Background::new()
+            .color_signal(hovered_signal.map_bool(|| NamedColor::Green5, || NamedColor::Green2))
+        )
+        .on_hovered_change(move |is_hovered| hovered.set(is_hovered))
+        .label("Change color")
+        .on_press(toggle_color)
+}
+
+// ------ ------
+//     Start
+// ------ ------
 
 #[wasm_bindgen(start)]
 pub fn start() {
     start_app("app", root);
 }
 
-// #![no_std]
-
-// use zoon::*;
-
-// blocks!{
-
-//     #[derive(Copy, Clone)]
-//     enum Color {
-//         Red,
-//         Blue,
-//     }
-
-//     #[s_var]
-//     fn color() -> Color {
-//         Color::A
-//     }
-
-//     #[update]
-//     fn toggle_color() {
-//         use Color::{Red, Blue};
-//         color().update(|color| if let Red = color { Blue } else { Red });
-//     }
-
-//     #[cache]
-//     fn fill_style() -> JsValue {
-//         let color = if let Color::Red = color.inner() { "red" } else { "blue" };
-//         JsValue::from(color)
-//     }
-
-//     #[el]
-//     fn root() -> Row {
-//         let fill_style = fill_style(); 
-//         row![
-//             canvas![
-//                 canvas::width(300),
-//                 canvas::height(300),
-//                 canvas::on_ready(|canvas| {
-//                     let ctx = canvas.context_2d();
-//                     ctx.lineWidth = 10;
-//                     fill_style.use_ref(|style| ctx.set_fill_style(style));
-//                     // Wall
-//                     ctx.stroke_rect(75., 140., 150., 110.);
-//                     // Door
-//                     ctx.fill_rect(130., 190., 40., 60.);
-//                     // Roof
-//                     ctx.begin_path();
-//                     ctx.move_to(50., 140.);
-//                     ctx.line_to(150., 60.);
-//                     ctx.line_to(250., 140.);
-//                     ctx.close_path();
-//                     ctx.stroke();
-//                 });
-//             ],
-//             button![
-//                 button::on_press(toggle_color),
-//                 "Change color",
-//             ]
-//         ]
-//     }
-
-// }
-
-// #[wasm_bindgen(start)]
-// pub fn start() {
-//     start!()
-// }
