d4a252c972f460deb80b3484d119dd3d4785d228
Martin Kavík <martin@kavik.cz>
2021-07-17 12:28:28 +0200

    header.rs

--- examples/pages/frontend/src/app.rs
+++ examples/pages/frontend/src/app.rs
@@ -1,5 +1,5 @@
 use zoon::*;
-use crate::{router::{router, Route}, report, login};
+use crate::{router::{router, Route}, report, login, header::header};
 
 
 // ------ ------
@@ -64,43 +64,6 @@ pub fn root() -> impl Element {
         .s(Spacing::new(20))
         .item(header())
         .item(page())
-}
-
-fn header() -> impl Element {
-    Row::new()
-        .s(Spacing::new(20))
-        .item(
-            Link::new()
-                .s(Font::new().underline().color(NamedColor::Blue7))
-                .label("Home")
-                .to(Route::Root)
-        )
-        .item(
-            Link::new()
-                .s(Font::new().underline().color(NamedColor::Blue7))
-                .label("Report")
-                .to(Route::Report)
-        )
-        .item_signal(
-            logged_user().signal_ref(|name| {
-                if let Some(name) = name {
-                    let (hovered, hovered_signal) = Mutable::new_and_signal(false);
-                    Button::new()
-                        .s(Background::new().color_signal(hovered_signal.map_bool(|| NamedColor::Red5, || NamedColor::Red2)))
-                        .s(Padding::new().x(7))
-                        .on_hovered_change(move |is_hovered| hovered.set(is_hovered))
-                        .label(format!("Log out {}", name))
-                        .on_press(log_out)
-                        .left_either()
-                } else {
-                    Link::new()
-                        .s(Font::new().underline().color(NamedColor::Blue7))
-                        .label("Log in")
-                        .to(Route::Login)
-                        .right_either()
-                }
-            })
-        )
 }
 
 fn page() -> impl Element {
