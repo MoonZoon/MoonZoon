263caad9c6a39a1120840e8b641b23278fc88710
Martin Kavík <martin@kavik.cz>
2021-08-11 01:49:40 +0200

    ability files

---
+++ crates/zoon/src/element/ability/keyboard_event_aware.rs
@@ -0,0 +1,48 @@
+use crate::*;
+use std::borrow::Borrow;
+
+pub trait KeyboardEventAware<T: RawEl>: UpdateRawEl<T> + Sized {
+    fn on_key_down(self, handler: impl FnOnce(KeyboardEvent) + Clone + 'static) -> Self {
+        self.update_raw_el(|raw_el| {
+            raw_el.event_handler(move |event: events::KeyDown| {
+                let keyboard_event = KeyboardEvent {
+                    key: Key::from(event.key()),
+                };
+                (handler.clone())(keyboard_event)
+            })
+        })
+    }
+}
+
+pub struct KeyboardEvent {
+    key: Key,
+}
+
+impl KeyboardEvent {
+    pub fn key(&self) -> &Key {
+        &self.key
+    }
+
+    pub fn if_key(&self, key: impl Borrow<Key>, f: impl FnOnce()) {
+        if &self.key == key.borrow() {
+            f()
+        }
+    }
+}
+
+#[derive(PartialEq, Eq)]
+pub enum Key {
+    Enter,
+    Escape,
+    Other(String),
+}
+
+impl From<String> for Key {
+    fn from(key: String) -> Self {
+        match key.as_ref() {
+            "Enter" => Key::Enter,
+            "Escape" => Key::Escape,
+            _ => Key::Other(key),
+        }
+    }
+}
---
+++ crates/zoon/src/element/ability/mouse_event_aware.rs
@@ -0,0 +1,27 @@
+use crate::*;
+
+pub trait MouseEventAware<T: RawEl>: UpdateRawEl<T> + Sized {
+    fn on_hovered_change(self, handler: impl FnOnce(bool) + Clone + 'static) -> Self {
+        let mouse_over_handler = move |hovered| (handler.clone())(hovered);
+        let mouse_leave_handler = mouse_over_handler.clone();
+        self.update_raw_el(|raw_el| {
+            raw_el
+                .event_handler(move |_: events_extra::MouseOver| mouse_over_handler(true))
+                .event_handler(move |_: events::MouseLeave| mouse_leave_handler(false))
+        })
+    }
+
+    fn on_click(self, handler: impl FnOnce() + Clone + 'static) -> Self {
+        let handler = move || handler.clone()();
+        self.update_raw_el(|raw_el| {
+            raw_el.event_handler(move |_: events::Click| handler())
+        })
+    }
+
+    fn on_double_click(self, handler: impl FnOnce() + Clone + 'static) -> Self {
+        let handler = move || handler.clone()();
+        self.update_raw_el(|raw_el| {
+            raw_el.event_handler(move |_: events::DoubleClick| handler())
+        })
+    }
+}
