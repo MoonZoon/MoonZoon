c729a00f97873330ce3cccce6087ddd94ef9aa4d
Martin Kavík <martin@kavik.cz>
2021-08-10 22:03:16 +0200

    Background::url, outline: none, border: none, default_icon

--- crates/zoon/src/element/checkbox.rs
+++ crates/zoon/src/element/checkbox.rs
@@ -188,10 +188,15 @@ impl<'a, IdFlag, OnChangeFlag, LabelFlag>
 // ------ ------
 
 pub fn default_icon(checked_signal: MutableSignal<bool>) -> impl Element {
+    // @TODO replace with better custom icons
+    // Icons from https://github.com/tastejs/todomvc
+    static ACTIVE_ICON: &str = "data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23ededed%22%20stroke-width%3D%223%22/%3E%3C/svg%3E";
+    static COMPLETED_ICON: &str = "data:image/svg+xml;utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20width%3D%2240%22%20height%3D%2240%22%20viewBox%3D%22-10%20-18%20100%20135%22%3E%3Ccircle%20cx%3D%2250%22%20cy%3D%2250%22%20r%3D%2250%22%20fill%3D%22none%22%20stroke%3D%22%23bddad5%22%20stroke-width%3D%223%22/%3E%3Cpath%20fill%3D%22%235dc2af%22%20d%3D%22M72%2025L42%2071%2027%2056l-4%204%2020%2020%2034-52z%22/%3E%3C/svg%3E";
+ 
     El::new()
-        .s(Background::new().color(NamedColor::Gray8))
-        .s(Font::new().size(16))
-        .s(Width::new(20))
-        .s(Height::new(20))
-        .child_signal(checked_signal.map_true(|| "V"))
+        .s(Width::new(40))
+        .s(Height::new(40))
+        .s(Background::new().url_signal(checked_signal.map_bool(
+            || COMPLETED_ICON, || ACTIVE_ICON
+        )))
 }
