f84a1885293712d71cac161bf6b2c8d5c8154eef
Martin Kavík <martin@kavik.cz>
2021-08-11 01:39:59 +0200

    AddNearbyElement signals

--- crates/zoon/src/element/ability.rs
+++ crates/zoon/src/element/ability.rs
@@ -161,65 +161,52 @@ pub trait MutableViewport<T: RawEl>: UpdateRawEl<T> + Sized {
 pub trait AddNearbyElement<'a>: UpdateRawEl<RawHtmlEl> + Sized {
     fn element_above(self, element: impl IntoOptionElement<'a> + 'a) -> Self {
         self.update_raw_el(|raw_el| {
-            raw_el
-                .child(
-                    RawHtmlEl::new("div")
-                        .style("position", "absolute")
-                        .style("bottom", "100%")
-                        .style("left", "0")
-                        .style("width", "100%")
-                        .style("pointer-events", "none")
-                        .attr("class", "above")
-                        .child(element)
-                )
+            raw_el.child(element_above_container().child(element))
+        })
+    }
+
+    fn element_above_signal(
+        self, 
+        element: impl Signal<Item = impl IntoOptionElement<'a>> + Unpin + 'static
+    ) -> Self {
+        self.update_raw_el(|raw_el| {
+            raw_el.child(element_on_above_container().child_signal(element))
         })
     }
 
     fn element_below(self, element: impl IntoOptionElement<'a> + 'a) -> Self {
         self.update_raw_el(|raw_el| {
-            raw_el
-                .child(
-                    RawHtmlEl::new("div")
-                        .style("position", "absolute")
-                        .style("top", "100%")
-                        .style("left", "0")
-                        .style("width", "100%")
-                        .style("pointer-events", "none")
-                        .attr("class", "below")
-                        .child(element)
-                )
+            raw_el.child(element_below_container().child(element))
+        })
+    }
+
+    fn element_on_below_signal(
+        self, 
+        element: impl Signal<Item = impl IntoOptionElement<'a>> + Unpin + 'static
+    ) -> Self {
+        self.update_raw_el(|raw_el| {
+            raw_el.child(element_on_below_container().child_signal(element))
         })
     }
 
     fn element_on_left(self, element: impl IntoOptionElement<'a> + 'a) -> Self {
         self.update_raw_el(|raw_el| {
-            raw_el
-                .child(
-                    RawHtmlEl::new("div")
-                        .style("position", "absolute")
-                        .style("right", "100%")
-                        .style("top", "0")
-                        .style("height", "100%")
-                        .style("pointer-events", "none")
-                        .attr("class", "on_left")
-                        .child(element)
-                )
+            raw_el.child(element_on_left_container().child(element))
+        })
+    }
+
+    fn element_on_left_signal(
+        self, 
+        element: impl Signal<Item = impl IntoOptionElement<'a>> + Unpin + 'static
+    ) -> Self {
+        self.update_raw_el(|raw_el| {
+            raw_el.child(element_on_left_container().child_signal(element))
         })
     }
 
     fn element_on_right(self, element: impl IntoOptionElement<'a> + 'a) -> Self {
         self.update_raw_el(|raw_el| {
-            raw_el
-                .child(
-                    RawHtmlEl::new("div")
-                        .style("position", "absolute")
-                        .style("left", "100%")
-                        .style("top", "0")
-                        .style("height", "100%")
-                        .style("pointer-events", "none")
-                        .attr("class", "on_right")
-                        .child(element)
-                )
+            raw_el.child(element_on_right_container().child(element))
         })
     }
 
@@ -228,17 +215,47 @@ pub trait AddNearbyElement<'a>: UpdateRawEl<RawHtmlEl> + Sized {
         element: impl Signal<Item = impl IntoOptionElement<'a>> + Unpin + 'static
     ) -> Self {
         self.update_raw_el(|raw_el| {
-            raw_el
-                .child(
-                    RawHtmlEl::new("div")
-                        .style("position", "absolute")
-                        .style("left", "100%")
-                        .style("top", "0")
-                        .style("height", "100%")
-                        .style("pointer-events", "none")
-                        .attr("class", "on_right")
-                        .child_signal(element)
-                )
+            raw_el.child(element_on_right_container().child_signal(element))
         })
     }
+}
+
+fn element_above_container() -> RawHtmlEl {
+    RawHtmlEl::new("div")
+        .style("position", "absolute")
+        .style("bottom", "100%")
+        .style("left", "0")
+        .style("width", "100%")
+        .style("pointer-events", "none")
+        .attr("class", "above")
+}
+
+fn element_below_container() -> RawHtmlEl {
+    RawHtmlEl::new("div")
+        .style("position", "absolute")
+        .style("top", "100%")
+        .style("left", "0")
+        .style("width", "100%")
+        .style("pointer-events", "none")
+        .attr("class", "below")
+}
+
+fn element_on_left_container() -> RawHtmlEl {
+    RawHtmlEl::new("div")
+        .style("position", "absolute")
+        .style("right", "100%")
+        .style("top", "0")
+        .style("height", "100%")
+        .style("pointer-events", "none")
+        .attr("class", "on_left")
+}
+
+fn element_on_right_container() -> RawHtmlEl {
+    RawHtmlEl::new("div")
+        .style("position", "absolute")
+        .style("left", "100%")
+        .style("top", "0")
+        .style("height", "100%")
+        .style("pointer-events", "none")
+        .attr("class", "on_right")
 }
