b8913ad35ba7514e6e2a4ceb8202510ebca21bea
Martin Kavík <martin@kavik.cz>
2021-08-19 12:50:26 +0200

    js benchmark example - remove fmt feature

--- examples/js-framework-benchmark/keyed/backend/src/main.rs
+++ examples/js-framework-benchmark/keyed/backend/src/main.rs
@@ -3,6 +3,7 @@ use moon::*;
 async fn frontend() -> Frontend {
     Frontend::new()
         .title("Benchmark example")
+        .default_styles(false)
         .append_to_head(r#"<link href="/_api/public/css/currentStyle.css" rel="stylesheet"/>"#)
         .body_content(r#"<div id="main"></div>"#)
 }
