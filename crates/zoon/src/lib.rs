e1052e6c0074a468abec1b417949bee636065d04
Martin Kavík <martin@kavik.cz>
2021-08-27 23:15:04 +0200

    time_tracker mocked getters

--- crates/zoon/src/lib.rs
+++ crates/zoon/src/lib.rs
@@ -74,7 +74,7 @@ pub use route_macro::route;
 pub use routing::{FromRouteSegments, RouteSegment, Router};
 
 #[cfg(feature = "moonlight")]
-pub use moonlight::{self, AuthToken, CorId};
+pub use moonlight::{self, AuthToken, CorId, EntityId, Wrapper};
 
 #[cfg(feature = "panic_hook")]
 pub use console_error_panic_hook;
