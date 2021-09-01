0ca189569bc886c119b8fc21ae8afccc65235982
Martin Kavík <martin@kavik.cz>
2021-09-01 13:23:30 +0200

    RoundedCorners Radius + _max

--- crates/zoon/src/style/rounded_corners.rs
+++ crates/zoon/src/style/rounded_corners.rs
@@ -1,98 +1,128 @@
 use crate::*;
 
-#[derive(Default)]
-pub struct RoundedCorners<'a> {
-    static_css_props: StaticCSSProps<'a>,
+// ------ Radius ------
+
+enum Radius {
+    Px(u32),
+    Max,
 }
 
-impl<'a> RoundedCorners<'a> {
+impl Default for Radius {
+    fn default() -> Self {
+        Self::Px(0)
+    }
+}
+
+// ------ RoundedCorners ------
+
+#[derive(Default)]
+pub struct RoundedCorners {
+    top_left: Radius,
+    top_right: Radius,
+    bottom_left: Radius,
+    bottom_right: Radius,
+}
+
+impl RoundedCorners {
     pub fn all(radius: u32) -> Self {
         Self::default()
             .top(radius)
             .bottom(radius)
     }
 
-    pub fn all_fully() -> Self {
+    pub fn all_max() -> Self {
         Self::default()
-            .top_fully()
-            .bottom_fully()
+            .top_max()
+            .bottom_max()
     }
 
     pub fn top(self, radius: u32) -> Self {
         self.top_left(radius).top_right(radius)
     }
 
-    pub fn top_fully(self) -> Self {
-        self.top_left_fully().top_right_fully()
+    pub fn top_max(self) -> Self {
+        self.top_left_max().top_right_max()
     }
 
     pub fn bottom(self, radius: u32) -> Self {
         self.bottom_left(radius).bottom_right(radius)
     }
 
-    pub fn bottom_fully(self) -> Self {
-        self.bottom_left_fully().bottom_right_fully()
+    pub fn bottom_max(self) -> Self {
+        self.bottom_left_max().bottom_right_max()
     }
 
     pub fn left(self, radius: u32) -> Self {
         self.top_left(radius).bottom_left(radius)
     }
 
-    pub fn left_fully(self) -> Self {
-        self.top_left_fully().bottom_left_fully()
+    pub fn left_max(self) -> Self {
+        self.top_left_max().bottom_left_max()
     }
 
     pub fn right(self, radius: u32) -> Self {
         self.top_right(radius).bottom_right(radius)
     }
 
-    pub fn right_fully(self) -> Self {
-        self.top_right_fully().bottom_right_fully()
+    pub fn right_max(self) -> Self {
+        self.top_right_max().bottom_right_max()
     }
 
     pub fn top_left(mut self, radius: u32) -> Self {
-        self.static_css_props
-            .insert("border-top-left-radius", px(radius));
+        self.top_left = Radius::Px(radius);
         self
     }
 
-    pub fn top_left_fully(self) -> Self {
-        self.top_left(9999)
+    pub fn top_left_max(mut self) -> Self {
+        self.top_left = Radius::Max;
+        self
     }
 
     pub fn top_right(mut self, radius: u32) -> Self {
-        self.static_css_props
-            .insert("border-top-right-radius", px(radius));
+        self.top_right = Radius::Px(radius);
         self
     }
 
-    pub fn top_right_fully(self) -> Self {
-        self.top_right(9999)
+    pub fn top_right_max(mut self) -> Self {
+        self.top_right = Radius::Max;
+        self
     }
 
     pub fn bottom_left(mut self, radius: u32) -> Self {
-        self.static_css_props
-            .insert("border-bottom-left-radius", px(radius));
+        self.bottom_left = Radius::Px(radius);
         self
     }
 
-    pub fn bottom_left_fully(self) -> Self {
-        self.bottom_left(9999)
+    pub fn bottom_left_max(mut self) -> Self {
+        self.bottom_left = Radius::Max;
+        self
     }
 
     pub fn bottom_right(mut self, radius: u32) -> Self {
-        self.static_css_props
-            .insert("border-bottom-right-radius", px(radius));
+        self.bottom_right = Radius::Px(radius);
         self
     }
 
-    pub fn bottom_right_fully(self) -> Self {
-        self.bottom_right(9999)
+    pub fn bottom_right_max(mut self) -> Self {
+        self.bottom_right = Radius::Max;
+        self
     }
 }
 
-impl<'a> Style<'a> for RoundedCorners<'a> {
+impl<'a> Style<'a> for RoundedCorners {
     fn into_css_props_container(self) -> CssPropsContainer<'a> {
-        CssPropsContainer::default().static_css_props(self.static_css_props)
+
+        // static_css_props: StaticCSSProps<'a>,
+        // self.static_css_props
+        //     .insert("border-top-left-radius", px(radius));
+        // self.static_css_props
+        //     .insert("border-top-right-radius", px(radius));
+        // self.static_css_props
+        //     .insert("border-bottom-left-radius", px(radius));
+        // self.static_css_props
+        //     .insert("border-bottom-right-radius", px(radius));
+
+        // CssPropsContainer::default().static_css_props(self.static_css_props)
+        CssPropsContainer::default()
     }
 }
