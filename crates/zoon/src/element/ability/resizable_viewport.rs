2958b8c4d80177ef91f6b7e7239910f25c80a493
Martin Kavík <martin@kavik.cz>
2021-09-01 12:47:20 +0200

    raw_el on_resize

--- crates/zoon/src/element/ability/resizable_viewport.rs
+++ crates/zoon/src/element/ability/resizable_viewport.rs
@@ -1,5 +1,4 @@
 use crate::*;
-use std::{rc::Rc, cell::Cell};
 
 type U32Width = u32;
 type U32Height = u32;
@@ -9,18 +8,6 @@ pub trait ResizableViewport<T: RawEl>: UpdateRawEl<T> + Sized {
         self,
         handler: impl FnOnce(U32Width, U32Height) + Clone + 'static,
     ) -> Self {
-        self.update_raw_el(|mut raw_el| {
-            let resize_observer = Rc::new(Cell::new(None));
-            let resize_observer_for_insert = Rc::clone(&resize_observer);
-
-            raw_el = raw_el.after_insert(move |ws_element| {
-                let observer = ResizeObserver::new(ws_element.as_ref(), handler);
-                resize_observer_for_insert.set(Some(observer));
-            });
-
-            raw_el.after_remove(move |_| {
-                drop(resize_observer);
-            })
-        })
+        self.update_raw_el(|raw_el| raw_el.on_resize(handler))
     }
 }
