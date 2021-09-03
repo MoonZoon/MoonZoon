df83a111fc292761916fabcf81ef7c2e420addeb
Martin Kavík <martin@kavik.cz>
2021-09-03 12:59:42 +0200

    time_tracker_page logic

--- examples/time_tracker/frontend/src/time_tracker_page.rs
+++ examples/time_tracker/frontend/src/time_tracker_page.rs
@@ -13,13 +13,13 @@ mod view;
 struct Client {
     id: ClientId,
     name:String,
-    projects: MutableVec<Arc<Project>>,
+    projects: Vec<Arc<Project>>,
 }
 
 #[derive(Default)]
 struct Project {
     id: ProjectId,
-    name: Mutable<String>,
+    name: String,
     time_entries: MutableVec<Arc<TimeEntry>>,
 }
 
@@ -47,95 +47,72 @@ fn clients() -> &'static MutableVec<Arc<Client>> {
 
 pub fn request_clients() {
     Task::start(async {
-        let msg = UpMsg::GetTimeBlocksClients;
+        let msg = UpMsg::GetTimeTrackerClients;
         if let Err(error) = connection().send_up_msg(msg).await {
-            eprintln!("get TimeBlocks clients request failed: {}", error);
+            eprintln!("get TimeTracker clients request failed: {}", error);
         }
     });
 }
 
 pub fn convert_and_set_clients(new_clients: Vec<time_tracker::Client>) {
-    // fn convert_clients(clients: Vec<time_blocks::Client>) -> Vec<Arc<Client>> {
-    //     clients.into_iter().map(|client| {
-    //         Arc::new(Client {
-    //             id: client.id,
-    //             name: client.name,
-    //             time_blocks: MutableVec::new_with_values(convert_time_blocks(client.time_blocks)),
-    //             tracked: client.tracked,
-    //         })
-    //     }).collect()
-    // }
-    // fn convert_time_blocks(time_blocks: Vec<time_blocks::TimeBlock>) -> Vec<Arc<TimeBlock>> {
-    //     time_blocks.into_iter().map(|time_block| {
-    //         Arc::new(TimeBlock {
-    //             id: time_block.id,
-    //             name: Mutable::new(time_block.name),
-    //             status: Mutable::new(time_block.status),
-    //             duration: Mutable::new(time_block.duration),
-    //             invoice: Mutable::new(time_block.invoice.map(convert_invoice)),
-    //             is_old: true,
-    //         })
-    //     }).collect()
-    // }
-    // fn convert_invoice(invoice: time_blocks::Invoice) -> Arc<Invoice> {
-    //     Arc::new(Invoice {
-    //         id: invoice.id,
-    //         custom_id: Mutable::new(invoice.custom_id),
-    //         url: Mutable::new(invoice.url),
-    //         is_old: true,
-    //     })
-    // }
-    // clients().lock_mut().replace_cloned(convert_clients(new_clients));
+    fn convert_clients(clients: Vec<time_tracker::Client>) -> Vec<Arc<Client>> {
+        clients.into_iter().map(|client| {
+            Arc::new(Client {
+                id: client.id,
+                name: client.name,
+                projects: convert_projects(client.projects),
+            })
+        }).collect()
+    }
+    fn convert_projects(time_blocks: Vec<time_tracker::Project>) -> Vec<Arc<Project>> {
+        time_blocks.into_iter().map(|project| {
+            Arc::new(Project {
+                id: project.id,
+                name: project.name,
+                time_entries: MutableVec::new_with_values(convert_time_entries(project.time_entries)),
+            })
+        }).collect()
+    }
+    fn convert_time_entries(time_entries: Vec<time_tracker::TimeEntry>) -> Vec<Arc<TimeEntry>> {
+        time_entries.into_iter().map(|time_entry| {
+            Arc::new(TimeEntry {
+                id: time_entry.id,
+                name: Mutable::new(time_entry.name),
+                started: Mutable::new(Wrapper::new(time_entry.started)),
+                stopped: Mutable::new(time_entry.stopped.map(Wrapper::new)),
+                is_old: true,
+            })
+        }).collect()
+    }
+    clients().lock_mut().replace_cloned(convert_clients(new_clients));
 }
 
-// -- time_block --
+// -- time_entry --
 
-// fn add_time_block(client: &Client) {
-//     // @TODO send up_msg
-//     client.time_blocks.lock_mut().insert_cloned(0, Arc::new(TimeBlock::default()))
-// }
+fn add_time_entry(project: &Project) {
+    // @TODO send up_msg
+    project.time_entries.lock_mut().insert_cloned(0, Arc::new(TimeEntry::default()));
+}
 
-// fn delete_time_block(client: &Client, time_block_id: TimeBlockId) {
-//     // @TODO send up_msg + confirm dialog
-//     client.time_blocks.lock_mut().retain(|time_block| time_block.id != time_block_id);
-// }
+fn delete_time_entry(project: &Project, time_entry_id: TimeEntryId) {
+    // @TODO send up_msg + confirm dialog
+    project.time_entries.lock_mut().retain(|time_entry| time_entry.id != time_entry_id);
+}
 
-// fn rename_time_block(time_block_id: TimeBlockId, name: &str) {
-//     // @TODO send up_msg
-//     zoon::println!("rename_time_block not implemented yet");
-// }
+fn rename_time_entry(time_entry_id: TimeEntryId, name: &str) {
+    // @TODO send up_msg
+    zoon::println!("rename_time_entry not implemented yet");
+}
 
-// fn set_time_block_status(time_block: &TimeBlock, status: TimeBlockStatus) {
-//     // @TODO send up_msg
-//     time_block.status.set(status);
-// }
+fn set_time_entry_started(time_entry: &TimeEntry, started: DateTime<Local>) {
+    // @TODO send up_msg
+    time_entry.started.set(Wrapper::new(started));
+}
 
-// fn set_time_block_duration(time_block: &TimeBlock, duration: Wrapper<Duration>) {
-//     // @TODO send up_msg
-//     time_block.duration.set(duration);
-// }
-
-// // -- invoice --
-
-// fn add_invoice(time_block: &TimeBlock) {
-//     // @TODO send up_msg
-//     time_block.invoice.set(Some(Arc::new(Invoice::default())));
-// }
-
-// fn delete_invoice(time_block: &TimeBlock) {
-//     // @TODO send up_msg + confirm dialog
-//     time_block.invoice.take();
-// }
-
-// fn set_invoice_custom_id(invoice_id: InvoiceId, custom_id: &str) {
-//     // @TODO send up_msg
-//     zoon::println!("set_invoice_custom_id not implemented yet");
-// }
-
-// fn set_invoice_url(invoice_id: InvoiceId, url: &str) {
-//     // @TODO send up_msg
-//     zoon::println!("set_invoice_url not implemented yet");
-// }
+fn set_time_entry_stopped(time_entry: &TimeEntry, stopped: DateTime<Local>) {
+    // @TODO send up_msg
+    time_entry.stopped.set(Some(Wrapper::new(stopped)));
+}
 
 // ------ ------
 //     View
@@ -144,8 +121,6 @@ pub fn convert_and_set_clients(new_clients: Vec<time_tracker::Client>) {
 pub fn view() -> RawElement {
     view::page().into_raw_element()
 }
-
-
 
 
 
