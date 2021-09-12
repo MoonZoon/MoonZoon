eb60817eda0855b7208fc6813325653eb7c02a4e
Martin Kavík <martin@kavik.cz>
2021-09-12 12:25:00 +0200

    on_focus, on_focused_change

--- crates/zoon/src/element/ability/focusable.rs
+++ crates/zoon/src/element/ability/focusable.rs
@@ -8,8 +8,21 @@ pub trait Focusable: UpdateRawEl<RawHtmlEl> + Sized {
         self
     }
 
+    fn on_focus(self, handler: impl FnOnce() + Clone + 'static) -> Self {
+        let handler = move || handler.clone()();
+        self.update_raw_el(|raw_el| raw_el.event_handler(move |_: events::Focus| handler()))
+    }
+
     fn on_blur(self, handler: impl FnOnce() + Clone + 'static) -> Self {
         let handler = move || handler.clone()();
         self.update_raw_el(|raw_el| raw_el.event_handler(move |_: events::Blur| handler()))
+    }
+
+    fn on_focused_change(self, handler: impl FnOnce(bool) + Clone + 'static) -> Self {
+        let focus_handler = move |focused| (handler.clone())(focused);
+        let blur_handler = focus_handler.clone();
+        self
+            .on_focus(move || focus_handler(true))
+            .on_blur(move || blur_handler(false))
     }
 }
