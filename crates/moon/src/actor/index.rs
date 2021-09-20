c827367a7c60f6ddcc40324d4ba5a28e143d12ac
Martin Kavík <martin@kavik.cz>
2021-09-20 13:04:27 +0200

    vscode settings, async-trait, Index wait_for

--- crates/moon/src/actor/index.rs
+++ crates/moon/src/actor/index.rs
@@ -1,6 +1,8 @@
 use crate::actor::{ActorId, PVar};
 use std::borrow::Borrow;
+use async_trait::async_trait;
 
+#[async_trait(?Send)]
 pub trait Index {
     type PVar: PVar;
     type Actor;
@@ -15,5 +17,18 @@ pub trait Index {
 
     fn for_each(&self, _f: impl FnMut(<Self::PVar as PVar>::Value, Self::Actor)) {
         todo!()
+    }
+
+    async fn wait_for(&self, key: impl Borrow<<Self::PVar as PVar>::Value> + 'static) -> Option<Self::Actor> {
+        // @TODO backoff + jitter + queue or something else?
+        let key = key.borrow();
+        for i in 0..10 {
+            let session = self.get(key);
+            if session.is_some() {
+                return session
+            }
+            tokio::time::sleep(tokio::time::Duration::from_millis(i * 200)).await;
+        }
+        None
     }
 }
