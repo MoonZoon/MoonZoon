ee7bfaa8f382b788832ca4b2c373a34785f5e8aa
Martin Kavík <martin@kavik.cz>
2021-10-18 00:15:34 +0200

    gray colors

--- crates/zoon/src/style/named_color.rs
+++ crates/zoon/src/style/named_color.rs
@@ -8,7 +8,16 @@ macro_rules! color {
 
 // The palette based on https://tailwindcss.com/docs/customizing-colors
 
+color!(GRAY_0 => 235.5, 22.1, 98.2);
+color!(GRAY_1 => 248.2, 18.5, 96.2);
+color!(GRAY_2 => 248.3, 16.7, 91.6);
+color!(GRAY_3 => 243.9, 14.4, 85.1);
+color!(GRAY_4 => 246.8, 12.5, 66.8);
+color!(GRAY_5 => 249.7, 15.7, 47.9);
+color!(GRAY_6 => 245, 25.3, 35.8);
 color!(GRAY_7 => 248.1, 32.2, 27.3);
+color!(GRAY_8 => 247.2, 42, 16.3);
+color!(GRAY_9 => 254, 48.3, 8.3);
 
 color!(GREEN_0 => 154.7, 66.4, 97.9);
 color!(GREEN_1 => 150.7, 63.8, 95.0);
