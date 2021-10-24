8194d1d099d44eaa03dda7f92de07814137fc3ba
Martin Kavík <martin@kavik.cz>
2021-10-24 12:00:35 +0200

    fix actix deps ; Rust 1.56.0 + edition 2021

---
+++ crates/moon_entry_macros/src/lib.rs
@@ -0,0 +1,42 @@
+use proc_macro::TokenStream;
+use quote::quote;
+
+/// Marks async main function as the Moon system entry-point.
+
+/// # Examples
+/// ```
+/// #[moon::main]
+/// async fn main() {
+///     async { println!("Hello world"); }.await
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn main(_: TokenStream, item: TokenStream) -> TokenStream {
+    let mut output: TokenStream = (quote! {
+        #[actix_web::rt::main(system = "actix_web::rt::System")]
+    })
+    .into();
+
+    output.extend(item);
+    output
+}
+
+/// Marks async test functions to use the Moon system entry-point.
+///
+/// # Examples
+/// ```
+/// #[moon::test]
+/// async fn test() {
+///     assert_eq!(async { "Hello world" }.await, "Hello world");
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn test(_: TokenStream, item: TokenStream) -> TokenStream {
+    let mut output: TokenStream = (quote! {
+        #[actix_web::rt::test(system = "actix_web::rt::System")]
+    })
+    .into();
+
+    output.extend(item);
+    output
+}
