c4511d79048a8a48d3cf3cba963a0a74c5dc22d6
Martin Kavík <martin@kavik.cz>
2021-11-20 15:37:50 +0100

    Timer::sleep

--- crates/zoon/src/timer.rs
+++ crates/zoon/src/timer.rs
@@ -4,14 +4,18 @@ use crate::*;
 
 pub struct Timer {
     handle: Option<JsHandle>,
-    on_tick: SendWrapper<Closure<dyn Fn()>>,
+    #[allow(dead_code)]
+    on_tick: SendWrapper<Closure<dyn FnMut()>>,
 }
 
 impl Timer {
     pub fn new(ms: u32, on_tick: impl FnOnce() + Clone + 'static) -> Self {
-        let mut this = Self::without_handle(on_tick);
-        this.handle = Some(JsHandle::Interval(set_interval(&this.on_tick, ms)));
-        this
+        let on_tick = move || (on_tick.clone())();
+        let on_tick = Closure::wrap(Box::new(on_tick) as Box<dyn FnMut()>);
+        Self {
+            handle: Some(JsHandle::Interval(set_interval(&on_tick, ms))),
+            on_tick: SendWrapper::new(on_tick),
+        }
     }
 
     pub fn new_immediate(ms: u32, on_tick: impl FnOnce() + Clone + 'static) -> Self {
@@ -19,19 +23,24 @@ impl Timer {
         Self::new(ms, on_tick)
     }
 
-    pub fn once(ms: u32, on_tick: impl FnOnce() + Clone + 'static) -> Self {
-        let mut this = Self::without_handle(on_tick);
-        this.handle = Some(JsHandle::Timeout(set_timeout(&this.on_tick, ms)));
-        this
-    }
-
-    fn without_handle(on_tick: impl FnOnce() + Clone + 'static) -> Self {
-        let on_tick = move || (on_tick.clone())();
-        let on_tick = Closure::wrap(Box::new(on_tick) as Box<dyn Fn()>);
+    pub fn once(ms: u32, on_tick: impl FnOnce() + 'static) -> Self {
+        let on_tick = Closure::once(on_tick);
         Self {
-            handle: None,
+            handle: Some(JsHandle::Timeout(set_timeout(&on_tick, ms))),
             on_tick: SendWrapper::new(on_tick),
         }
+    }
+
+    pub async fn sleep(ms: u32) {
+        let (sender, receiver) = oneshot::channel();
+        let _timer = Self::once(ms, move || {
+            sender
+                .send(())
+                .expect_throw("`sender` failed in `Timer::sleep`")
+        });
+        receiver
+            .await
+            .expect_throw("`receiver` failed in `Timer::sleep`")
     }
 }
 
@@ -56,13 +65,13 @@ enum JsHandle {
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_name = setInterval)]
-    fn set_interval(callback: &Closure<dyn Fn()>, ms: u32) -> f64;
+    fn set_interval(callback: &Closure<dyn FnMut()>, ms: u32) -> f64;
 
     #[wasm_bindgen(js_name = clearInterval)]
     fn clear_interval(token: f64);
 
     #[wasm_bindgen(js_name = setTimeout)]
-    fn set_timeout(callback: &Closure<dyn Fn()>, ms: u32) -> f64;
+    fn set_timeout(callback: &Closure<dyn FnMut()>, ms: u32) -> f64;
 
     #[wasm_bindgen(js_name = clearTimeout)]
     fn clear_timeout(token: f64);
