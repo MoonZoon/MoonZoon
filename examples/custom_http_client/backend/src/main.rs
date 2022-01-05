46ea3fb72499eb55743b59dfe44e32d31e8e155d
Martin Kavík <martin@kavik.cz>
2022-01-05 15:30:52 +0100

    example custom_http_client

--- examples/custom_http_client/backend/src/main.rs
+++ examples/custom_http_client/backend/src/main.rs
@@ -1,28 +1,56 @@
+use moon::actix_web::{get, HttpResponse, Responder};
 use moon::*;
-use moon::actix_web::{get, Responder};
 
 async fn frontend() -> Frontend {
-    Frontend::new().title("Custom HTTP client example").append_to_head(
-        "
+    Frontend::new()
+        .title("Custom HTTP client example")
+        .append_to_head(
+            "
         <style>
             html {
                 background-color: black;
                 color: lightgray;
             }
         </style>",
-    )
+        )
 }
 
 async fn up_msg_handler(_: UpMsgRequest<()>) {}
 
-#[get("_api/hello")]
-async fn hello() -> impl Responder {
-    "Hello!"
+#[get("_api/moonzoon_stars")]
+async fn moonzoon_stars() -> impl Responder {
+    #[derive(Debug, Deserialize)]
+    #[serde(crate = "serde")]
+    struct GithubResponse {
+        stargazers_count: u32,
+    }
+
+    async fn stars_request() -> reqwest::Result<u32> {
+        reqwest::Client::builder()
+            // https://docs.github.com/en/rest/overview/resources-in-the-rest-api#user-agent-required
+            .user_agent("MoonZoon")
+            .build()?
+            .get("https://api.github.com/repos/MoonZoon/MoonZoon")
+            .send()
+            .await?
+            .json::<GithubResponse>()
+            .await
+            .map(|github_response| github_response.stargazers_count)
+    }
+
+    stars_request().await.map_or_else(
+        |error| {
+            eprintln!("Error: {:#?}", error);
+            HttpResponse::InternalServerError().finish()
+        },
+        |stars| HttpResponse::Ok().body(stars.to_string()),
+    )
 }
 
 #[moon::main]
 async fn main() -> std::io::Result<()> {
-    start(frontend, up_msg_handler, |cfg|{
-        cfg.service(hello);
-    }).await
+    start(frontend, up_msg_handler, |cfg| {
+        cfg.service(moonzoon_stars);
+    })
+    .await
 }
--- examples/custom_http_client/frontend/src/lib.rs
+++ examples/custom_http_client/frontend/src/lib.rs
@@ -1,22 +1,76 @@
-use zoon::{*, println, routing::origin};
+use zoon::{eprintln, routing::origin, *};
 
-fn send_request() {
-    Task::start(async {
-        let url = origin() + "/_api/hello";
-        let greeting = reqwest::get(url)
-            .await.unwrap_throw()
+// ------ ------
+//     Types
+// ------ ------
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Stars {
+    Loading,
+    Loaded(u32),
+    Failed,
+}
+
+// ------ ------
+//    States
+// ------ ------
+
+#[static_ref]
+fn stars() -> &'static Mutable<Stars> {
+    Mutable::new(Stars::Loading)
+}
+
+// ------ ------
+//   Commands
+// ------ ------
+
+fn load_stars() {
+    async fn stars_request() -> reqwest::Result<u32> {
+        // @TODO remove `origin()` once a wasm-compatible HTTP client supports relative urls
+        // Surf issue: https://github.com/http-rs/surf/issues/314
+        // Reqwest issue: https://github.com/seanmonstar/reqwest/issues/988
+        Ok(reqwest::get(origin() + "/_api/moonzoon_stars")
+            .await?
+            .error_for_status()?
             .text()
-            .await.unwrap_throw();
-        println!("Greeting: '{}'", greeting);
+            .await?
+            .parse()
+            .unwrap_throw())
+    }
+    Task::start(async {
+        stars().set_neq(Stars::Loading);
+        match stars_request().await {
+            Ok(loaded_stars) => stars().set_neq(Stars::Loaded(loaded_stars)),
+            Err(error) => {
+                eprintln!("stars request failed: {:#?}", error);
+                stars().set_neq(Stars::Failed)
+            }
+        }
     });
 }
 
+// ------ ------
+//     View
+// ------ ------
+
 fn root() -> impl Element {
-    Column::new()
-        .item(Button::new().label("Send request").on_press(send_request))
+    Row::new().item("MoonZoon stars: ").item(stars_text())
 }
+
+fn stars_text() -> impl Element {
+    Text::with_signal(stars().signal().map(|stars| match stars {
+        Stars::Loading => "Loading...".into_cow_str(),
+        Stars::Loaded(stars) => stars.into_cow_str(),
+        Stars::Failed => "Loading failed!".into_cow_str(),
+    }))
+}
+
+// ------ ------
+//     Start
+// ------ ------
 
 #[wasm_bindgen(start)]
 pub fn start() {
     start_app("app", root);
+    load_stars();
 }
