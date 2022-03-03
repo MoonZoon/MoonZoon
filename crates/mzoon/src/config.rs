efa18840522c40dc8811afd57050726e799fe75e
Martin Kavík <martin@kavik.cz>
2022-03-03 16:26:38 +0100

    toml_to_env_vars

--- crates/mzoon/src/config.rs
+++ crates/mzoon/src/config.rs
@@ -3,7 +3,8 @@ use fehler::throws;
 use log::LevelFilter;
 use serde::Deserialize;
 use tokio::fs;
-use std::collections::{BTreeMap, VecDeque};
+use std::collections::VecDeque;
+use std::rc::Rc;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -15,7 +16,7 @@ pub struct Config {
     pub cors: Cors,
     pub watch: Watch,
     #[serde(skip)]
-    pub custom_env_vars: BTreeMap<String, String>,
+    pub custom_env_vars: Vec<(String, String)>,
 }
 
 impl Config {
@@ -56,39 +57,53 @@ pub struct Watch {
 }
 
 // #[throws]
-fn toml_to_env_vars(toml: toml::Value) -> anyhow::Result<BTreeMap<String, String>> {
-    println!("{toml:#?}");
+fn toml_to_env_vars(toml: toml::Value) -> anyhow::Result<Vec<(String, String)>> {
+    println!("{toml:#}");
 
-    let mut vars = BTreeMap::<String, String>::new();
+    let mut vars = Vec::<(String, String)>::new();
 
-    struct StackItem<'a> {
-        parent_name: String,
-        name: &'a String,
-        toml_value: &'a toml::Value
+    struct StackItem {
+        parent_name: Option<Rc<String>>,
+        name: Option<String>,
+        toml_value: toml::Value
     }
-    let empty_string = String::new();
 
     let mut stack = VecDeque::<StackItem>::new();
     let root = StackItem {
-        parent_name: String::new(),
-        name: &empty_string,
-        toml_value: &toml
+        parent_name: None,
+        name: None,
+        toml_value: toml
     };
     stack.push_back(root);
 
     while let Some(StackItem { parent_name, name, toml_value }) = stack.pop_front() {
-        let string_value = match toml_value {
+        let env_name = match (parent_name, name) {
+            (Some(parent_name), Some(name)) => {
+                Some(format!("{parent_name}_{name}"))
+            }
+            (None, Some(name)) => Some(name),
+            (None, None) => None,
+            (Some(_), None) => unreachable!(),
+        };
+        let env_value = match toml_value {
             toml::Value::Table(table) => {
-                let parent_name = format!("{parent_name}_{name}");
-                stack.extend(table.iter().map(|(name, toml_value)| {
-                    StackItem { parent_name: parent_name.clone(), name, toml_value }
+                let parent_name = env_name.map(Rc::new);
+                stack.extend(table.into_iter().map(|(name, toml_value)| {
+                    StackItem { 
+                        parent_name: parent_name.clone(), 
+                        name: Some(name.to_ascii_uppercase()), 
+                        toml_value 
+                    }
                 }));
                 continue;
             }
-            value => value.clone().try_into_string()?
-
+            value => value.try_into_string()?
         };
-        vars.insert(format!("{parent_name}_{name}"), string_value);
+        if let Some(env_name) = env_name {
+            vars.push((env_name, env_value));
+        } else {
+            unreachable!();
+        }
     }
 
     println!("{vars:#?}");
@@ -102,7 +117,7 @@ trait TryIntoString {
 impl TryIntoString for toml::Value {
     fn try_into_string(self) -> anyhow::Result<String> {
         let string_value = match self {
-            toml::Value::Table(_) => bail!("TOML table cannot be stringified"),
+            toml::Value::Table(_) => bail!("TOML tables cannot be stringified"),
             toml::Value::Boolean(value) => value.to_string(),
             toml::Value::Float(value) => value.to_string(),
             toml::Value::Integer(value) => value.to_string(),
