c6f1ab39f509ce15516e2b924d10f6056c9eb6ff
Martin Kavík <martin@kavik.cz>
2022-03-03 21:46:41 +0100

    config refactor, remove heck

--- crates/mzoon/src/config.rs
+++ crates/mzoon/src/config.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Error, Result};
+use anyhow::{Context, Error};
 use fehler::throws;
 use log::LevelFilter;
 use serde::Deserialize;
@@ -38,26 +38,41 @@ pub struct Watch {
 impl Config {
     #[throws]
     pub async fn load_from_moonzoon_tomls() -> Config {
-        let config_toml = fs::read_to_string("MoonZoon.toml")
-            .await
-            .context("Failed to read MoonZoon.toml")?;
-        let mut config = toml::from_str(&config_toml).context("Failed to parse MoonZoon.toml")?;
-
-        if fs::metadata("MoonZoonCustom.toml").await.is_err() {
-            return config;
+        let mut config = read_moonzoon_toml().await?;
+        if let Some(custom_env_vars) = read_moonzoon_custom_toml().await? {
+            println!("custom_env_vars: {custom_env_vars:#?}");
+            config.custom_env_vars = custom_env_vars;
         }
-        let custom_config_toml = fs::read_to_string("MoonZoonCustom.toml")
-            .await
-            .context("Failed to read MoonZoonCustom.toml")?;
-        let custom_config = toml::from_str(&custom_config_toml).context("Failed to parse MoonZoonCustom.toml")?;
-        config.custom_env_vars = toml_to_env_vars(custom_config).context("Failed to parse MoonZoonCustom.toml's content")?;
-        println!("{:#?}", config.custom_env_vars);
         config
     }
 }
 
-fn toml_to_env_vars(toml: toml::Value) -> Result<Vec<(String, String)>> {
-    println!("{toml:#}");
+#[throws]
+async fn read_moonzoon_toml() -> Config {
+    let config_toml = fs::read_to_string("MoonZoon.toml")
+        .await
+        .context("Failed to read MoonZoon.toml")?;
+
+    toml::from_str(&config_toml).context("Failed to parse MoonZoon.toml")?
+}
+
+#[throws]
+async fn read_moonzoon_custom_toml() -> Option<Vec<(String, String)>> {
+    if fs::metadata("MoonZoonCustom.toml").await.is_err() {
+        return None;
+    }
+
+    let custom_config_toml = fs::read_to_string("MoonZoonCustom.toml")
+        .await
+        .context("Failed to read MoonZoonCustom.toml")?;
+
+    let custom_config = toml::from_str(&custom_config_toml).context("Failed to parse MoonZoonCustom.toml")?;
+    let pairs = toml_to_env_vars(custom_config).context("Failed to parse MoonZoonCustom.toml's content")?;
+    Some(pairs)
+} 
+
+#[throws]
+fn toml_to_env_vars(toml: toml::Value) -> Vec<(String, String)> {
     tree_into_pairs(
         toml,
         |parent_name, original_name| format!("{parent_name}_{original_name}"),
@@ -65,11 +80,14 @@ fn toml_to_env_vars(toml: toml::Value) -> Result<Vec<(String, String)>> {
             match toml {
                 toml::Value::Table(table) => {
                     Ok(NodeContent::Children(Box::new(
-                        table.into_iter().map(|(name, value)| (name.to_ascii_uppercase(), value))
+                        table.into_iter().map(|(mut name, value)| {
+                            name.make_ascii_uppercase();
+                            (name, value)
+                        })
                     )))
                 }
                 value => Ok(NodeContent::Value(value.try_into_string()?))
             }
         },
-    )
+    )?
 }
