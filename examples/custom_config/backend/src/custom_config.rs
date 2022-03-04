1d5ffeee9df47f52dd6bfdc42b4e1220d497dcbb
Martin Kavík <martin@kavik.cz>
2022-03-04 14:39:40 +0100

    custom_config.rs complete, removed facebook settings

--- examples/custom_config/backend/src/custom_config.rs
+++ examples/custom_config/backend/src/custom_config.rs
@@ -1,62 +1,50 @@
 use moon::*;
 use std::collections::BTreeSet;
 
-// @TODO return `Result` from `from_env_vars`?
+// @TODO improve API & refactor the code below and:
+// @TODO return `Result` from `from_env_vars`? +(rename?)
 // @TODO write a derive macro `FromEnvVars`?
 
 pub static CUSTOM_CONFIG: Lazy<CustomConfig> = Lazy::new(CustomConfig::from_env_vars);
 
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 #[serde(crate = "serde")]
 pub struct CustomConfig {
     pub my_api: String,
     pub pi: f32,
     pub favorite_languages: BTreeSet<String>,
+    pub is_pig_pink: bool,
+    pub birthday: DateTime<Local>,
+    #[serde(default = "Postgres::from_env_vars")]
+    pub postgres: Postgres
 }
 
 impl FromEnvVars for CustomConfig {
     const ENTITY_NAME: &'static str = "CustomConfig";
 }
 
-// #[derive(Debug, Deserialize)]
-// #[serde(default)]
-// pub struct Redirect {
-//     // REDIRECT_PORT
-//     pub port: u16,
-//     // REDIRECT_ENABLED
-//     pub enabled: bool,
-// }
+#[derive(Debug, Deserialize)]
+#[serde(crate = "serde")]
+pub struct Postgres {
+    #[serde(default = "PostgresUser::from_env_vars")]
+    pub user: PostgresUser,
+    pub host: String,
+    pub name: String,
+}
 
-// impl FromEnvVars for Redirect {
-//     const ENTITY_NAME: &'static str = "Redirect";
-//     const ENV_PREFIX: &'static str = "REDIRECT_";
-// }
+impl FromEnvVars for Postgres {
+    const ENTITY_NAME: &'static str = "Postgres";
+    const ENV_PREFIX: &'static str = "POSTGRES_";
+}
 
-// impl Default for Redirect {
-//     fn default() -> Self {
-//         Self {
-//             port: 8081,
-//             enabled: false,
-//         }
-//     }
-// }
+#[derive(Debug, Deserialize)]
+#[serde(crate = "serde")]
+pub struct PostgresUser {
+    pub name: String,
+    pub password: String,
+}
 
-// #[derive(Debug, Deserialize)]
-// #[serde(default)]
-// pub struct Cors {
-//     // CORS_ORIGINS="http://localhost:8080,http://127.0.0.1:8080,*"
-//     pub origins: BTreeSet<Cow<'static, str>>,
-// }
-
-// impl FromEnvVars for Cors {
-//     const ENTITY_NAME: &'static str = "Cors";
-//     const ENV_PREFIX: &'static str = "CORS_";
-// }
-
-// impl Default for Cors {
-//     fn default() -> Self {
-//         Self {
-//             origins: BTreeSet::from_iter(["*".into()]),
-//         }
-//     }
-// }
+impl FromEnvVars for PostgresUser {
+    const ENTITY_NAME: &'static str = "PostgresUser";
+    const ENV_PREFIX: &'static str = "POSTGRES_USER_";
+}
