77931ba2d630553e2fc950f18b5815bbdd189450
arn-the-long-beard <arn.menant@gmail.com>
2022-03-15 18:16:06 +0100

    doc: added examples and documentation for Scrollbars

--- crates/zoon/src/style/scrollbars.rs
+++ crates/zoon/src/style/scrollbars.rs
@@ -1,26 +1,68 @@
 use crate::*;
 
+/// Style an element with scrollbars.
 #[derive(Default)]
 pub struct Scrollbars<'a> {
     static_css_props: StaticCSSProps<'a>,
 }
 
 impl<'a> Scrollbars<'a> {
+    /// Add horizontal and vertical scrollbars if needed.
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let paragraph = Paragraph::new()
+    ///     .content(
+    ///         "Lorem ipsum dolor sit amet,
+    /// consectetur adipiscing elit. Donec placerat lacus in commodo molestie.",
+    ///     )
+    ///     .s(Scrollbars::both())
+    ///     .s(Height::new(100))
+    ///     .s(Width::new(50));
+    /// ```
     pub fn both() -> Self {
         let mut this = Self::default();
         this.static_css_props.insert("overflow", "auto");
         this
     }
 
-    /// https://css-tricks.com/popping-hidden-overflow/
+    /// Add only the horizontal scrollbar.
+    /// More information available at <https://css-tricks.com/popping-hidden-overflow/>
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let paragraph = Paragraph::new()
+    ///     .content(
+    ///         "Lorem ipsum dolor sit amet,
+    /// consectetur adipiscing elit. Donec placerat lacus in commodo molestie.",
+    ///     )
+    ///     .s(Scrollbars::x_and_clip_y())
+    ///     .s(Height::new(100))
+    ///     .s(Width::new(50));
+    /// ```
     pub fn x_and_clip_y() -> Self {
         let mut this = Self::default();
         this.static_css_props.insert("overflow-x", "auto");
         this.static_css_props.insert("overflow-y", "hidden");
         this
     }
-
-    /// https://css-tricks.com/popping-hidden-overflow/
+    /// Add only the vertical scrollbar.
+    /// More information available at <https://css-tricks.com/popping-hidden-overflow/>
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let paragraph = Paragraph::new()
+    ///     .content(
+    ///         "Lorem ipsum dolor sit amet,
+    /// consectetur adipiscing elit. Donec placerat lacus in commodo molestie.",
+    ///     )
+    ///     .s(Scrollbars::y_and_clip_x())
+    ///     .s(Height::new(100))
+    ///     .s(Width::new(50));
+    /// ```
     pub fn y_and_clip_x() -> Self {
         let mut this = Self::default();
         this.static_css_props.insert("overflow-y", "auto");
