37861a8f88d1c557fefaf964d15fc2f059005c77
Martin Kavík <martin@kavik.cz>
2022-03-16 08:58:26 +0100

    HSLuv updaters

--- examples/layers/frontend/src/lib.rs
+++ examples/layers/frontend/src/lib.rs
@@ -1,6 +1,7 @@
 use zoon::{*, named_color::*};
 use LayerId::*;
 
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum LayerId {
     A,
     B,
@@ -12,36 +13,37 @@ fn layer_order() -> &'static Mutable<[LayerId; 3]> {
     Mutable::new([A, B, C])
 }
 
+fn bring_to_front(layer_id: LayerId) {
+    let mut layers = layer_order().lock_mut();
+    let layer_position = layers.iter().position(|id| id == &layer_id).unwrap_throw();
+    layers.swap(0, layer_position);
+}
+
 fn root() -> impl Element {
     Stack::new()
         .s(Align::center())
         .s(Width::new(180))
         .s(Height::new(180))
-        .layer(layer_a())
-        .layer(layer_b())
-        .layer(layer_c())
-}
-
-fn layer_a() -> impl Element {
-    rectangle(A, RED_6, Align::new())
-}
-
-fn layer_b() -> impl Element {
-    rectangle(B, GREEN_6, Align::center())
-}
-
-fn layer_c() -> impl Element {
-    rectangle(C, BLUE_6, Align::new().bottom().right())
+        .layer(rectangle(A, RED_6, Align::new()))
+        .layer(rectangle(B, GREEN_6, Align::center()))
+        .layer(rectangle(C, BLUE_6, Align::new().bottom().right()))
 }
 
 fn rectangle(layer_id: LayerId, color: HSLuv, align: Align) -> impl Element {
+    let (hovered, hovered_signal) = Mutable::new_and_signal(false);
     El::new()
         .s(Width::new(100))
         .s(Height::new(100))
-        .s(Background::new().color(color))
         .s(RoundedCorners::all(15))
-        .s(Borders::all(Border::new()))
+        .s(Cursor::new(CursorIcon::Pointer))
+        .s(Shadows::new([Shadow::new().blur(20).color(GRAY_8)]))
+        .s(Background::new().color_signal(hovered_signal.map_bool(
+            move || color.update_l(|l| l + 5.), 
+            move || color,
+        )))
         .s(align)
+        .on_hovered_change(move |is_hovered| hovered.set_neq(is_hovered))
+        .on_click(move || bring_to_front(layer_id))
 }
 
 #[wasm_bindgen(start)]
