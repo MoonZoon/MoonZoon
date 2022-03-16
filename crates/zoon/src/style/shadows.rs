b12d64b3c831ac085a2aa5df594e37dc5bb5af61
arn-the-long-beard <arn.menant@gmail.com>
2022-03-16 16:57:58 +0100

    doc: added examples and documentation for Shadows

--- crates/zoon/src/style/shadows.rs
+++ crates/zoon/src/style/shadows.rs
@@ -3,13 +3,33 @@ use std::borrow::Cow;
 
 // ------ Shadows ------
 
+/// Style to add shadows to an element.
 #[derive(Default)]
 pub struct Shadows<'a> {
+    /// Static css properties used by zoon.
     static_css_props: StaticCSSProps<'a>,
+    /// Customizable css properties which can be added.
     dynamic_css_props: DynamicCSSProps,
 }
 
 impl<'a> Shadows<'a> {
+    /// Add new shadows.
+    /// The method accepts an iterator of [Shadow] so multiple effects can be
+    /// used. More information at <https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow>
+    /// # Example
+    /// ```no_run
+    /// use zoon::{named_color::*, *};
+    ///
+    /// let button = Button::new()
+    ///     .s(Shadows::new([Shadow::new()
+    ///         .color(RED_3)
+    ///         .y(10)
+    ///         .x(10)
+    ///         .blur(5)]))
+    ///     .s(Width::new(50))
+    ///     .s(Height::new(50))
+    ///     .label("Click me");
+    /// ```
     pub fn new(shadows: impl IntoIterator<Item = Shadow>) -> Self {
         let shadows = shadows
             .into_iter()
@@ -20,7 +40,24 @@ impl<'a> Shadows<'a> {
         this.static_css_props.insert("box-shadow", shadows);
         this
     }
-
+    /// Add new shadows depending of signal's state..
+    /// The method accepts an iterator of [Shadow] so multiple effects can be
+    /// used.
+    /// # Example
+    /// ```no_run
+    /// use zoon::{named_color::*, *};
+    ///
+    /// let (hovered, hover_signal) = Mutable::new_and_signal(false);
+    /// let button = Button::new()
+    ///     .s(Shadows::with_signal(hover_signal.map_bool(
+    ///         || [Shadow::new().color(RED_8).y(10).x(10)],
+    ///         || [Shadow::new().color(RED_3).y(10).x(10).blur(5)],
+    ///     )))
+    ///     .s(Width::new(50))
+    ///     .s(Height::new(50))
+    ///     .on_hovered_change(move |h| hovered.set(h))
+    ///     .label("Click me");
+    /// ```
     pub fn with_signal(
         shadows: impl Signal<Item = impl IntoIterator<Item = Shadow>> + Unpin + 'static,
     ) -> Self {
@@ -73,6 +110,8 @@ impl<'a> Style<'a> for Shadows<'a> {
 
 // ------ Shadow ------
 
+/// Single Shadow effect.
+/// NMore information available at <https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow>.
 #[derive(Default)]
 pub struct Shadow {
     inner: bool,
@@ -84,35 +123,113 @@ pub struct Shadow {
 }
 
 impl Shadow {
+    /// Create a new default shadow, which is not visible.
     pub fn new() -> Self {
         Self::default()
     }
 
+    /// Set the shadow to appear inside the
+    /// element on its top left side.
+    /// # Example
+    /// ```no_run
+    /// use zoon::{named_color::*, *};
+    ///
+    /// let button = Button::new()
+    ///     .s(Shadows::new([Shadow::new()
+    ///         .color(RED_3)
+    ///         .inner()
+    ///         .y(10)
+    ///         .x(10)]))
+    ///     .s(Width::new(50))
+    ///     .s(Height::new(50))
+    ///     .label("Click me");
+    /// ```
     pub fn inner(mut self) -> Self {
         self.inner = true;
         self
     }
 
+    /// Set the horizontal distance for the offset.
+    /// # Example
+    /// ```no_run
+    /// use zoon::{named_color::*, *};
+    ///
+    /// let button = Button::new()
+    ///     .s(Shadows::new([Shadow::new().color(RED_3).inner().x(10)]))
+    ///     .s(Width::new(50))
+    ///     .s(Height::new(50))
+    ///     .label("Click me");
+    /// ```
     pub fn x(mut self, x: i32) -> Self {
         self.x = x;
         self
     }
 
+    /// Set the vertical distance for the offset.
+    /// # Example
+    /// ```no_run
+    /// use zoon::{named_color::*, *};
+    ///
+    /// let button = Button::new()
+    ///     .s(Shadows::new([Shadow::new().color(RED_3).y(10)]))
+    ///     .s(Width::new(50))
+    ///     .s(Height::new(50))
+    ///     .label("Click me");
+    /// ```
     pub fn y(mut self, y: i32) -> Self {
         self.y = y;
         self
     }
 
+    /// Increase the size of the shadow.
+    /// Using a negative value will decrease
+    /// it.
+    /// # Example
+    /// ```no_run
+    /// use zoon::{named_color::*, *};
+    ///
+    /// let button = Button::new()
+    ///     .s(Shadows::new([Shadow::new().color(RED_3).spread(10)]))
+    ///     .s(Width::new(50))
+    ///     .s(Height::new(50))
+    ///     .label("Click me");
+    /// ```
     pub fn spread(mut self, spread: i32) -> Self {
         self.spread = spread;
         self
     }
 
+    /// Add blur radius for the shadow..
+    /// # Example
+    /// ```no_run
+    /// use zoon::{named_color::*, *};
+    ///
+    /// let button = Button::new()
+    ///     .s(Shadows::new([Shadow::new().color(RED_3).blur(10)]))
+    ///     .s(Width::new(50))
+    ///     .s(Height::new(50))
+    ///     .label("Click me");
+    /// ```
     pub fn blur(mut self, blur: u32) -> Self {
         self.blur = blur;
         self
     }
 
+    /// Set the color for the shadow.
+    /// # Example
+    /// ```no_run
+    /// use zoon::{named_color::*, *};
+    ///
+    /// let button = Button::new()
+    ///     .s(Shadows::new([Shadow::new()
+    ///         .color(RED_3)
+    ///         .inner()
+    ///         .x(10)
+    ///         .y(10)]))
+    ///     .s(Width::new(50))
+    ///     .s(Height::new(50))
+    ///     .label("Click me");
+    /// ```
     pub fn color(mut self, color: impl Into<Option<HSLuv>>) -> Self {
         if let Some(color) = color.into() {
             self.color = Some(color);
