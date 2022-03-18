adc4b11b700bec6fce45a36bd6ea59d52050367d
arn-the-long-beard <arn.menant@gmail.com>
2022-03-18 11:03:17 +0100

    doc: added an example and documentation for Spacing

--- crates/zoon/src/style/spacing.rs
+++ crates/zoon/src/style/spacing.rs
@@ -1,11 +1,26 @@
 use crate::*;
 
+/// Define space between elements.
+/// More information at <https://developer.mozilla.org/en-US/docs/Web/CSS/gap>.
 #[derive(Default)]
 pub struct Spacing<'a> {
+    /// Static css properties used by zoon.
     static_css_props: StaticCSSProps<'a>,
 }
 
 impl<'a> Spacing<'a> {
+    /// Add space in pixels between elements. It needs to be set on the
+    /// container.
+    /// More information at <https://developer.mozilla.org/en-US/docs/Web/CSS/gap>.
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let grid = Row::new()
+    ///     .s(Spacing::new(10))
+    ///     .item(Column::new().item("first column"))
+    ///     .item(Column::new().item("second column"));
+    /// ```
     pub fn new(spacing: u32) -> Self {
         let mut this = Self::default();
         this.static_css_props.insert("gap", px(spacing));
