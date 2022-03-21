2cfe02b26ee0153d53fba112ebc4784a469f6f24
arn-the-long-beard <arn.menant@gmail.com>
2022-03-21 12:07:06 +0100

    doc: added example and documentation for Height

--- crates/zoon/src/style/width.rs
+++ crates/zoon/src/style/width.rs
@@ -2,7 +2,9 @@ use crate::*;
 
 #[derive(Default)]
 pub struct Width<'a> {
+    /// Static css properties used by zoon.
     static_css_props: StaticCSSProps<'a>,
+    /// Customizable css properties which can be added.
     dynamic_css_props: DynamicCSSProps,
     width_mode: WidthMode,
 }
@@ -19,6 +21,13 @@ impl Default for WidthMode {
 }
 
 impl<'a> Width<'a> {
+    /// Define the width with pixels for an element.
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let button = Button::new().s(Width::new(50)).label("Click ne");
+    /// ```
     pub fn new(width: u32) -> Self {
         let mut this = Self::default();
         this.static_css_props.insert("width", px(width));
@@ -26,6 +35,17 @@ impl<'a> Width<'a> {
         this
     }
 
+    /// Define the width with pixels depending of signal's state.
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let (is_hovered, hover_signal) = Mutable::new_and_signal(false);
+    /// let button = Button::new()
+    ///     .s(Width::with_signal(hover_signal.map_bool(|| 50, || 100)))
+    ///     .on_hovered_change(move |hover| is_hovered.set(hover))
+    ///     .label("hover me");
+    /// ```
     pub fn with_signal(
         width: impl Signal<Item = impl Into<Option<u32>>> + Unpin + 'static,
     ) -> Self {
@@ -37,6 +57,14 @@ impl<'a> Width<'a> {
         this
     }
 
+    /// Define the width with the number of `0` characters with its current
+    /// font. More information at <https://stackoverflow.com/questions/48649169/what-is-difference-between-css-em-and-ch-units>.
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let button = Button::new().s(Width::zeros(4)).label("Click ne");
+    /// ```
     pub fn zeros(zeros: u32) -> Self {
         let mut this = Self::default();
         this.static_css_props.insert("width", ch(zeros));
@@ -44,6 +72,15 @@ impl<'a> Width<'a> {
         this
     }
 
+    /// Set the element width to fill its container or parent element.
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let button = Button::new()
+    ///     .s(Width::fill())
+    ///     .label("Hover this giant button");
+    /// ```
     pub fn fill() -> Self {
         let mut this = Self::default();
         this.static_css_props.insert("width", "100%");
@@ -51,16 +88,41 @@ impl<'a> Width<'a> {
         this
     }
 
+    /// Set the element minimum width.
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let container = Column::new()
+    ///     .s(Width::new(5))
+    ///     .item(Button::new().s(Width::default().min(25)).label("Click ne"));
+    /// ```
     pub fn min(mut self, width: u32) -> Self {
         self.static_css_props.insert("min-width", px(width));
         self
     }
 
+    /// Set the element maximum width.
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let button = Button::new().s(Width::default().max(25)).label("Click ne");
+    /// ```
     pub fn max(mut self, width: u32) -> Self {
         self.static_css_props.insert("max-width", px(width));
         self
     }
 
+    /// Set the maximum element width to fill its container.
+    /// # Example
+    /// ```no_run
+    /// use zoon::*;
+    ///
+    /// let button = Button::new()
+    ///     .s(Width::default().max_fill())
+    ///     .label("Hover this giant button");
+    /// ```
     pub fn max_fill(mut self) -> Self {
         self.static_css_props.insert("max-width", "100%");
         self
