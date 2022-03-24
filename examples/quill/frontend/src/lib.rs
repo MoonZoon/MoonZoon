771eba2a3a72a64a9ce7a9c888ab9481dc8a5b81
arn-the-long-beard <arn.menant@gmail.com>
2022-03-24 13:22:48 +0100

    example: added a demo to integrate with a javascript library

---
+++ examples/quill/frontend/src/lib.rs
@@ -0,0 +1,27 @@
+use zoon::*;
+
+fn root() -> impl Element {
+    Column::new()
+        .item(
+            El::new()
+                .update_raw_el(|el| el.attr("class", "standalone-container"))
+                .child(
+                    El::new().update_raw_el(|el| el.attr("id", "snow-container")),
+                ),
+        )
+        .item(El::with_tag(Tag::Custom("script")).child(
+            r#"
+  var quill = new Quill('#snow-container', {
+    placeholder: 'Compose an epic...',
+    theme: 'snow'
+  });
+"#,
+        ))
+}
+
+// ---------- // -----------
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    start_app("app", root);
+}
