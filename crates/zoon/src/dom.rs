f3c6cd4e3487c3a5bf2a60a21056de908a7d8aa0
Martin Kavík <martin@kavik.cz>
2022-03-27 21:40:04 +0200

    reexported dom, dom::append_to_head

--- crates/zoon/src/dom.rs
+++ crates/zoon/src/dom.rs
@@ -11,3 +11,7 @@ pub fn document() -> web_sys::Document {
 pub fn history() -> web_sys::History {
     window().history().unwrap_throw()
 }
+
+pub fn append_to_head(html: impl AsRef<str>) {
+    
+}
