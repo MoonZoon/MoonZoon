f2b9193e92d8ed60636eeb12e4c50b57a4bda9c2
Martin Kavík <martin@kavik.cz>
2022-03-27 17:41:35 +0200

    text_editor.rs

--- examples/js_text_editor/frontend/src/lib.rs
+++ examples/js_text_editor/frontend/src/lib.rs
@@ -1,5 +1,7 @@
 use zoon::*;
-use std::rc::Rc;
+
+mod text_editor;
+use text_editor::TextEditor;
 
 // ------ ------
 //    States
@@ -21,21 +23,15 @@ fn root() -> impl Element {
 }
 
 fn text_editor() -> impl Element {
-    fn format_contents(json: JsString) -> Option<String> {
-        let json = json.as_string()?;
-        let json: serde_json::Value = serde_json::from_str(&json).ok()?;
-        Some(format!("{json:#}"))
-    }
-
-    let on_change = Rc::new(Closure::wrap(Box::new(|json: JsString| {
-        contents().set(format_contents(json)) 
-    }) as Box<dyn Fn(JsString)>));
-
+    let text_editor = Mutable::new(None);
     El::new()
-        .after_insert(clone!((on_change) move |html_element| {
-            externs::QuillController::new(html_element.into()).on_change(&on_change);
+        .after_insert(clone!((text_editor) move |html_element| {
+            let editor = TextEditor::new(html_element).on_change(|json| {
+                contents().set(json.map(|json| format!("{json:#}")));
+            });
+            text_editor.set(Some(editor));
         }))
-        .after_remove(|_| drop(on_change))
+        .after_remove(|_| drop(text_editor))
 }
 
 fn contents_display() -> impl Element {
@@ -43,24 +39,6 @@ fn contents_display() -> impl Element {
         .s(Padding::all(10))
         .s(Font::new().family([FontFamily::Monospace]))
         .child_signal(contents().signal_cloned())
-}
-
-// ------ ------
-//    Externs
-// ------ ------
-
-mod externs {
-    use super::*;
-    #[wasm_bindgen(module = "/js/quill_controller.js")]
-    extern "C" {
-        pub type QuillController;
-
-        #[wasm_bindgen(constructor)]
-        pub fn new(element: JsValue) -> QuillController;
-
-        #[wasm_bindgen(method)]
-        pub fn on_change(this: &QuillController, on_change: &Closure<dyn Fn(JsString)>);
-    }
 }
 
 // ------ ------
