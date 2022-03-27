c528c717541add6b091aaf6a74931f96bc9a74a8
Martin Kavík <martin@kavik.cz>
2022-03-27 22:36:55 +0200

    dom head, body, load_stylesheet, load_script

--- crates/zoon/src/dom.rs
+++ crates/zoon/src/dom.rs
@@ -12,6 +12,36 @@ pub fn history() -> web_sys::History {
     window().history().unwrap_throw()
 }
 
-pub fn append_to_head(html: impl AsRef<str>) {
-    
+pub fn head() -> web_sys::HtmlHeadElement {
+    document().head().unwrap_throw()
+}
+
+pub fn body() -> web_sys::HtmlBodyElement {
+    document().body().unwrap_throw().unchecked_into()
+}
+
+pub fn load_stylesheet(url: impl AsRef<str>) {
+    let link: web_sys::HtmlLinkElement = document()
+        .create_element("link")
+        .unwrap_throw()
+        .unchecked_into();
+
+    link.set_attribute("href", url.as_ref()).unwrap_throw();
+    link.set_attribute("rel", "stylesheet").unwrap_throw();
+
+    head().append_child(&link).unwrap_throw();
+}
+
+pub fn load_script(url: impl AsRef<str>) {
+    // Note: `script` cannot be added dynamically through `set_inner_html` (silent fail).
+
+    let script: web_sys::HtmlScriptElement = document()
+        .create_element("script")
+        .unwrap_throw()
+        .unchecked_into();
+
+    script.set_attribute("src", url.as_ref()).unwrap_throw();
+    script.set_attribute("type", "text/javascript").unwrap_throw();
+
+    head().append_child(&script).unwrap_throw();
 }
