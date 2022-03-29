7769f69924ff76f6e2e1728b561c1d56128d7ef3
arn-the-long-beard <arn.menant@gmail.com>
2022-03-29 12:30:35 +0200

    doc: improved example in styleable.rs
    Co-authored-by: Martin Kavík <martin@kavik.cz>

--- crates/zoon/src/element/ability/styleable.rs
+++ crates/zoon/src/element/ability/styleable.rs
@@ -36,8 +36,7 @@ pub trait Styleable<'a, T: RawEl>: UpdateRawEl<T> + Sized {
     /// ```no_run
     /// use zoon::{named_color::*, *};
     ///
-    /// let (hovered, hover_signal) = Mutable::new_and_signal(false);
-    /// let (hovered_2, hover_signal_2) = Mutable::new_and_signal(false);
+    /// let hovered = Mutable::new(false);
     /// Column::new().s(Height::screen()).item(
     ///     Button::new()
     ///         .s(Align::center())
@@ -46,15 +45,12 @@ pub trait Styleable<'a, T: RawEl>: UpdateRawEl<T> + Sized {
     ///         .s(Font::new()
     ///             .size(30)
     ///             .center()
-    ///             .color_signal(hover_signal.map_bool(|| PINK_5, || PINK_7)))
+    ///             .color_signal(hovered.signal().map_bool(|| PINK_5, || PINK_7)))
     ///         .s(Background::new()
-    ///             .color_signal(hover_signal_2.map_bool(|| GREEN_2, || GREEN_7)))
-    ///         .on_hovered_change(move |is_hovered| {
-    ///             hovered.set(is_hovered);
-    ///             hovered_2.set(is_hovered)
-    ///         })
+    ///             .color_signal(hovered.signal().map_bool(|| GREEN_7, || GREEN_2)))
+    ///         .on_hovered_change(move |is_hovered| hovered.set(is_hovered))
     ///         .label("Hover me"),
-    /// );
+    /// )
     /// ```
     fn s(self, style: impl Style<'a>) -> Self {
         self.update_raw_el(|raw_el| style.apply_to_raw_el(raw_el, None).0)
