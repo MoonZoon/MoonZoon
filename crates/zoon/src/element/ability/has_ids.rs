807e40f1624df2744bea5d4e87514f7d594f0edf
Martin Kavík <martin@kavik.cz>
2022-04-22 20:56:02 +0200

    CheckBox & TextInput - allow to set both id and hidden_label

--- crates/zoon/src/element/ability/has_ids.rs
+++ crates/zoon/src/element/ability/has_ids.rs
@@ -1,16 +1,16 @@
 use crate::*;
 
 pub trait HasIds: UpdateRawEl + Sized {
+    fn id<'a>(self, id: impl IntoCowStr<'a>) -> Self {
+        self.update_raw_el(move |raw_el| {
+            raw_el.id(id)
+        })
+    }
+
     fn class_id(self, consumer: impl FnOnce(ClassId)) -> Self {
         self.update_raw_el(move |raw_el| {
             consumer(raw_el.class_id());
             raw_el
-        })
-    }
-
-    fn id<'a>(self, id: impl IntoCowStr<'a>) -> Self {
-        self.update_raw_el(move |raw_el| {
-            raw_el.id(id)
         })
     }
 }
