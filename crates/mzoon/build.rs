3de8c23b9aad33e6b5a747254d11afaba46b677c
Martin Kavík <martin@kavik.cz>
2022-06-07 10:31:47 +0200

    create_new_project_tar + mzoon/new_project

--- crates/mzoon/build.rs
+++ crates/mzoon/build.rs
@@ -1,5 +1,5 @@
-use ignore::Walk;
-use std::env;
+use ignore::WalkBuilder;
+use std::{env, ffi::OsStr, fs::File, path::Path};
 
 // https://doc.rust-lang.org/cargo/reference/build-scripts.html
 
@@ -10,20 +10,34 @@ macro_rules! instruction {
 }
 
 // https://github.com/rust-lang/cargo/issues/985
-macro_rules! warning {
-    ($($arg: tt)*) => {
-        instruction!("cargo:warning={}", format!($($arg)*))
-    }
-}
+// macro_rules! warning {
+//     ($($arg: tt)*) => {
+//         instruction!("cargo:warning={}", format!($($arg)*))
+//     }
+// }
 
 fn main() {
     instruction!("cargo:rustc-env=TARGET={}", env::var("TARGET").unwrap());
+    create_new_project_tar();
+}
 
-    for entry in Walk::new("../../examples/new_project") {
-        let entry = entry.unwrap();
-        if entry.file_name() == "Makefile.toml" {
+fn create_new_project_tar() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let file = File::create(Path::new(&out_dir).join("new_project.tar")).unwrap();
+    let mut tar_builder = tar::Builder::new(file);
+
+    let new_project_path = Path::new("new_project");
+    let extra_ignored_files = [OsStr::new("Makefile.toml"), OsStr::new("Cargo.lock")];
+
+    for entry in WalkBuilder::new(new_project_path).hidden(false).build() {
+        let path = entry.unwrap().into_path();
+        if path.is_dir() || extra_ignored_files.contains(&path.file_name().unwrap()) {
             continue;
         }
-        warning!("{:#?}", entry.path());
+        let tar_path = path.strip_prefix(new_project_path).unwrap();
+        tar_builder
+            .append_file(&tar_path, &mut File::open(&path).unwrap())
+            .unwrap();
     }
+    tar_builder.finish().unwrap();
 }
--- crates/mzoon/src/command/new.rs
+++ crates/mzoon/src/command/new.rs
@@ -1,8 +1,13 @@
 use anyhow::Error;
 use fehler::throws;
 use std::path::PathBuf;
+use tar::Archive;
+
+static NEW_PROJECT_TAR: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/new_project.tar"));
 
 #[throws]
 pub async fn new(path: PathBuf) {
-    println!("NEW PATH: '{path:#?}'");
+    let mut new_project_tar = Archive::new(NEW_PROJECT_TAR);
+    new_project_tar.unpack(path)?;
+    print!("New project created");
 }
