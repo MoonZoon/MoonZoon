c1af4e95515d1a7a6c36acbb6ec481dafba1a55a
Martin Kavík <martin@kavik.cz>
2022-06-09 19:20:40 +0200

    fix frontend-dist on linux - retry download

--- crates/mzoon/src/helper/download.rs
+++ crates/mzoon/src/helper/download.rs
@@ -3,7 +3,13 @@ use fehler::throws;
 
 #[throws]
 pub async fn download(url: impl AsRef<str>) -> Vec<u8> {
-    reqwest::get(url.as_ref())
+    let url = url.as_ref();
+    again::retry(|| get(url)).await?
+}
+
+#[throws]
+async fn get(url: &str) -> Vec<u8> {
+    reqwest::get(url)
         .await?
         .error_for_status()?
         .bytes()
