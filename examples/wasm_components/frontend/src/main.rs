a7ceb5ba755bd51a88504a1475acf150618a8222
Martin Kavík <martin@kavik.cz>
2022-11-21 20:16:31 +0100

    example wasm_components with wasmer

--- examples/wasm_components/frontend/src/main.rs
+++ examples/wasm_components/frontend/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::anyhow;
-use wasmi::{core::F64, Caller, Engine, Extern, Func, Linker, Module, Store};
+use std::sync::{Arc, Mutex};
+use wasmer::{imports, Function, FunctionEnv, FunctionEnvMut, Instance, Module, Store, WasmPtr};
 use zoon::{eprintln, named_color::*, println, *};
 
 #[static_ref]
@@ -12,7 +13,10 @@ fn component_said() -> &'static Mutable<Option<String>> {
     Mutable::new(None)
 }
 
-// WARNING: Both `Wasmi` host and Wasm components use low-level communication mechanisms with `unsafe`
+type Size = i32;
+type Ptr = i32;
+
+// WARNING: Both `Wasmer` host and Wasm components use low-level communication mechanisms with `unsafe`
 // with direct memory manipulation. A library/bindgen should be used for communication in the future.
 async fn load_and_use_component(file_list: web_sys::FileList) -> anyhow::Result<()> {
     let file_bytes = file_list
@@ -24,91 +28,91 @@ async fn load_and_use_component(file_list: web_sys::FileList) -> anyhow::Result<
         .apply_ref(js_sys::Uint8Array::new)
         .to_vec();
 
-    let engine = Engine::default();
-    let module = Module::new(&engine, file_bytes.as_slice())?;
+    let mut store = Store::default();
+    let module = Module::new(&store, file_bytes).await?;
 
-    type HostState = ();
-    let mut store = Store::new(&engine, ());
+    #[derive(Clone, Default)]
+    struct EnvData {
+        instance: Arc<Mutex<Option<SendWrapper<Arc<Instance>>>>>,
+    }
 
-    let host_log = Func::wrap(
-        &mut store,
-        |caller: Caller<'_, HostState>, ptr: u32, size: u32| {
-            let result = (|| {
-                let memory = caller
-                    .get_export("memory")
-                    .and_then(Extern::into_memory)
-                    .ok_or_else(|| anyhow!("could not find memory \"memory\""))?;
+    let env_data = EnvData::default();
+    let env = FunctionEnv::new(&mut store, env_data.clone());
 
-                let mut message = vec![0; size as usize];
-                memory
-                    .read(caller, ptr as usize, message.as_mut_slice())
-                    .map_err(wasmi::Error::Memory)?;
-                let message = String::from_utf8(message)?;
+    fn host_log(env: FunctionEnvMut<EnvData>, ptr: WasmPtr<u8>, size: Size) {
+        let result: anyhow::Result<()> = (|| {
+            let instance_lock = env
+                .data()
+                .instance
+                .lock()
+                .map_err(|error| anyhow!("failed to lock `Instance`: {error:#}"))?;
+            let instance = instance_lock
+                .as_ref()
+                .ok_or_else(|| anyhow!("instance cannot be `None`"))?;
+            let memory = instance.exports.get_memory("memory")?;
 
-                println!("{message}");
-                Ok::<(), anyhow::Error>(())
-            })();
-            if let Err(error) = result {
-                eprintln!("{error:#}");
-            }
-        },
-    );
+            let memory_view = memory.view(&env);
+            let message = ptr.read_utf8_string(&memory_view, size.try_into()?)?;
+            println!("{message}");
+            Ok(())
+        })();
+        if let Err(error) = result {
+            eprintln!("host_log failed: {error:#?}");
+        }
+    }
 
-    let mut linker = Linker::<HostState>::new();
-    linker.define("env", "host_log", host_log)?;
+    let import_object = imports! {
+        "env" => {
+            "host_log" => Function::new_typed_with_env(&mut store, &env, host_log)
+        }
+    };
 
-    let instance = linker.instantiate(&mut store, &module)?.start(&mut store)?;
+    let instance = Arc::new(Instance::new(&mut store, &module, &import_object).await?);
+    env_data
+        .instance
+        .lock()
+        .map_err(|error| anyhow!("failed to lock `Instance`: {error:#}"))?
+        .replace(SendWrapper::new(instance.clone()));
 
+    let memory = instance.exports.get_memory("memory")?;
     let alloc = instance
-        .get_export(&store, "alloc")
-        .and_then(Extern::into_func)
-        .ok_or_else(|| anyhow!("could not find function \"alloc\""))?
-        .typed::<u32, u32>(&store)?;
-
+        .exports
+        .get_typed_function::<Size, WasmPtr<u8>>(&store, "alloc")?;
     let free = instance
-        .get_export(&store, "free")
-        .and_then(Extern::into_func)
-        .ok_or_else(|| anyhow!("could not find function \"free\""))?
-        .typed::<(u32, u32), ()>(&store)?;
-
-    let memory = instance
-        .get_export(&mut store, "memory")
-        .and_then(Extern::into_memory)
-        .ok_or_else(|| anyhow!("could not find memory \"memory\""))?;
+        .exports
+        .get_typed_function::<(Ptr, Size), ()>(&store, "free")?;
 
     let mut new_component_said = String::new();
     // call `sum`
     {
         let sum = instance
-            .get_export(&store, "sum")
-            .and_then(Extern::into_func)
-            .ok_or_else(|| anyhow!("could not find function \"sum\""))?
-            .typed::<(F64, F64), F64>(&mut store)?;
+            .exports
+            .get_typed_function::<(f64, f64), f64>(&store, "sum")?;
 
         let a = 1.2;
         let b = 3.4;
-        let sum_a_b = sum.call(&mut store, (a.into(), b.into()))?;
+        let sum_a_b = sum.call(&mut store, a, b)?;
 
         new_component_said.push_str(&format!("\n{a} + {b} = {sum_a_b}"));
     }
     // call `sum_array`
     {
         let sum_array = instance
-            .get_export(&store, "sum_array")
-            .and_then(Extern::into_func)
-            .ok_or_else(|| anyhow!("could not find function \"sum_array\""))?
-            .typed::<(u32, u32), F64>(&mut store)?;
+            .exports
+            .get_typed_function::<(Ptr, Size), f64>(&store, "sum_array")?;
 
         let addends = vec![1.25, 2.5, 3.1, 60.];
         let addends_serialized = bincode::serialize(&addends)?;
-        let addends_size = addends_serialized.len() as u32;
+        let addends_size: Size = addends_serialized.len().try_into()?;
 
         let ptr = alloc.call(&mut store, addends_size)?;
+
         memory
-            .write(&mut store, ptr as usize, &addends_serialized)
-            .map_err(wasmi::Error::Memory)?;
-        let addends_sum = sum_array.call(&mut store, (ptr, addends_size))?;
-        free.call(&mut store, (ptr, addends_size))?;
+            .view(&store)
+            .write(ptr.offset().into(), &addends_serialized)?;
+        let addends_sum = sum_array.call(&mut store, ptr.offset().try_into()?, addends_size)?;
+
+        free.call(&mut store, ptr.offset().try_into()?, addends_size)?;
 
         new_component_said.push_str(&format!("\nSum {addends:?} = {addends_sum}"));
     }
