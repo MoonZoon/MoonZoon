d5580d16c393fdceac3794dcbcad4e281ffa2502
Martin Kavík <martin@kavik.cz>
2023-08-19 14:20:37 +0200

    map_key

--- crates/futures_signals_ext/src/signal_map_ext_ext.rs
+++ crates/futures_signals_ext/src/signal_map_ext_ext.rs
@@ -25,6 +25,18 @@ pub trait SignalMapExtExt: SignalMapExt {
             callback: PhantomData,
         }
     }
+
+    #[inline]
+    fn map_key<A, F>(self, callback: F) -> MapKey<Self, F>
+    where
+        F: FnMut(Self::Key) -> A,
+        Self: Sized,
+    {
+        MapKey {
+            signal: self,
+            callback,
+        }
+    }
 }
 
 impl<S: SignalMapExt> SignalMapExtExt for S {}
@@ -46,5 +58,55 @@ impl<S: SignalMap, F: FnMut(MapDiff<S::Key, S::Value>)> Future for ForEachSync<S
     #[inline]
     fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
         self.project().future.poll(cx)
+    }
+}
+
+// -- MapKey --
+
+#[pin_project(project = MapKeyProj)]
+#[derive(Debug)]
+#[must_use = "SignalMaps do nothing unless polled"]
+pub struct MapKey<A, B> {
+    #[pin]
+    signal: A,
+    callback: B,
+}
+
+impl<A, B, F> SignalMap for MapKey<A, F>
+where
+    A: SignalMap,
+    F: FnMut(A::Key) -> B,
+{
+    type Key = B;
+    type Value = A::Value;
+
+    // TODO should this inline ?
+    #[inline]
+    fn poll_map_change(
+        self: Pin<&mut Self>,
+        cx: &mut Context,
+    ) -> Poll<Option<MapDiff<Self::Key, Self::Value>>> {
+        let MapKeyProj { signal, callback } = self.project();
+
+        signal.poll_map_change(cx).map(|some| {
+            some.map(|change| {
+                match change {
+                    // TODO figure out a more efficient way of implementing this
+                    MapDiff::Replace { entries } => MapDiff::Replace {
+                        entries: entries.into_iter().map(|(k, v)| (callback(k), v)).collect(),
+                    },
+                    MapDiff::Insert { key, value } => MapDiff::Insert {
+                        key: callback(key),
+                        value,
+                    },
+                    MapDiff::Update { key, value } => MapDiff::Update {
+                        key: callback(key),
+                        value,
+                    },
+                    MapDiff::Remove { key } => MapDiff::Remove { key: callback(key) },
+                    MapDiff::Clear {} => MapDiff::Clear {},
+                }
+            })
+        })
     }
 }
