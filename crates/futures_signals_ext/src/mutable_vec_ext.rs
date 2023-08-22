8f443433f477e46149dec72189d45a4655c40633
Martin Kavík <martin@kavik.cz>
2023-08-22 23:17:29 +0200

    MutableVecExt::take

--- crates/futures_signals_ext/src/mutable_vec_ext.rs
+++ crates/futures_signals_ext/src/mutable_vec_ext.rs
@@ -9,6 +9,14 @@ pub trait MutableVecExt<T>: private::MutableVecExt<T> {
     fn use_ref(&self, f: impl FnOnce(&MutableVecLockRef<T>)) {
         f(&self.lock_ref())
     }
+
+    fn take(&self) -> Vec<T> {
+        // @TODO better way (`Mutable.0` + `mem::take`?) (`futures-signals` update needed?)
+        let mut lock = self.lock_mut();
+        let new_vec = lock.drain(..).collect();
+        lock.shrink_to_fit();
+        new_vec
+    }
 }
 impl<T> MutableVecExt<T> for MutableVec<T> {}
 
