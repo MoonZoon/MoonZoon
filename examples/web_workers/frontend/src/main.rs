e329e6ca0ee9467250f16b384eda991fb7f4afb5
Martin Kavík <martin@kavik.cz>
2023-09-05 12:20:42 +0200

    mzoon - reenable help, new_project resolver = "2"

--- examples/web_workers/frontend/src/main.rs
+++ examples/web_workers/frontend/src/main.rs
@@ -5,7 +5,7 @@ use frontend::*;
 // Worker examples have been copied from the `gloo` GitHub repo and modified.
 
 // @TODO automatize / improve API / etc
-// @TODO don't forget to add extra `wasm-bidngen` arguments for workers, process it through `wasm-opt` and compress it (uncomment the related code in Moon)
+// @TODO don't forget to add extra `wasm-bindgen` arguments for workers, process it through `wasm-opt` and compress it (uncomment the related code in Moon)
 // @TODO make it compatible with `--frontend-dist` and `mzoon new` (don't forget to add `resolver = "2"` and update `.gitignore` everywhere)
 
 fn main() {
