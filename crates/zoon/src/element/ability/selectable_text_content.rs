bac4ef551d5c11794630943830448ddcc6e616b5
Martin Kavík <martin@kavik.cz>
2023-09-23 22:58:29 +0200

    text_content_selecting_signal + Options; remove Height from Counter

--- crates/zoon/src/element/ability/selectable_text_content.rs
+++ crates/zoon/src/element/ability/selectable_text_content.rs
@@ -3,8 +3,23 @@ use crate::*;
 // ------ SelectableTextContent ------
 
 pub trait SelectableTextContent: RawElWrapper + Sized {
-    fn text_content_selecting(self, selecting: TextContentSelecting) -> Self {
-        self.update_raw_el(|raw_el| raw_el.style("user-select", selecting.user_select))
+    fn text_content_selecting(self, selecting: impl Into<Option<TextContentSelecting>>) -> Self {
+        if let Some(selecting) = selecting.into() {
+            return self.update_raw_el(|raw_el| raw_el.style("user-select", selecting.user_select));
+        }
+        self
+    }
+
+    fn text_content_selecting_signal(
+        self,
+        selecting: impl Signal<Item = impl Into<Option<TextContentSelecting>>> + Unpin + 'static,
+    ) -> Self {
+        self.update_raw_el(|raw_el| {
+            raw_el.style_signal(
+                "user-select",
+                selecting.map(|selecting| selecting.into().map(|selecting| selecting.user_select)),
+            )
+        })
     }
 }
 
@@ -18,13 +33,17 @@ pub struct TextContentSelecting {
 
 impl Default for TextContentSelecting {
     fn default() -> Self {
-        Self {
-            user_select: "auto",
-        }
+        Self::auto()
     }
 }
 
 impl TextContentSelecting {
+    pub fn auto() -> Self {
+        Self {
+            user_select: "auto",
+        }
+    }
+
     pub fn none() -> Self {
         Self {
             user_select: "none",
@@ -35,10 +54,11 @@ impl TextContentSelecting {
         Self { user_select: "all" }
     }
 
-    // @TODO uncomment of remove?
-    // pub fn text() -> Self {
-    //     Self { user_select: "text" }
-    // }
+    pub fn text() -> Self {
+        Self {
+            user_select: "text",
+        }
+    }
 
     pub fn contain() -> Self {
         Self {
--- examples/counter/frontend/src/main.rs
+++ examples/counter/frontend/src/main.rs
@@ -22,7 +22,6 @@ fn counter_button(label: &str, step: i32) -> impl Element {
     let (hovered, hovered_signal) = Mutable::new_and_signal(false);
     Button::new()
         .s(Width::exact(45))
-        .s(Height::exact(25))
         .s(RoundedCorners::all_max())
         .s(Background::new()
             .color_signal(hovered_signal.map_bool(|| hsluv!(300, 75, 85), || hsluv!(300, 75, 75))))
