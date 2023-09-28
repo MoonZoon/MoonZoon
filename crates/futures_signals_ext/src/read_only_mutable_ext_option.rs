c877d0dcbed857bc83a270b528326007ac9151f4
Martin Kavík <martin@kavik.cz>
2023-09-28 11:37:25 +0200

    wait_for_some_ref returns value, wait_for_some_cloned

--- crates/futures_signals_ext/src/read_only_mutable_ext_option.rs
+++ crates/futures_signals_ext/src/read_only_mutable_ext_option.rs
@@ -2,30 +2,30 @@ use crate::futures_signals::signal::MutableSignalRef;
 use crate::*;
 
 pub trait ReadOnlyMutableExtOption<T>: private::ReadOnlyMutableExtOption<T> {
-    fn wait_for_some_ref(
+    fn wait_for_some_ref<U: 'static>(
         &self,
-        f: impl for<'f> FnOnce(&'f T) + 'static,
-    ) -> future::LocalBoxFuture<'static, ()>;
+        f: impl for<'f> FnOnce(&'f T) -> U + 'static,
+    ) -> future::LocalBoxFuture<'static, U>;
+
+    fn wait_for_some_cloned(&self) -> future::LocalBoxFuture<'static, T>
+    where
+        T: Clone + 'static,
+    {
+        self.wait_for_some_ref(Clone::clone)
+    }
 }
 
 impl<T: 'static> ReadOnlyMutableExtOption<T> for ReadOnlyMutable<Option<T>> {
-    fn wait_for_some_ref(
+    fn wait_for_some_ref<U: 'static>(
         &self,
-        f: impl for<'f> FnOnce(&'f T) + 'static,
-    ) -> future::LocalBoxFuture<'static, ()> {
+        f: impl for<'f> FnOnce(&'f T) -> U + 'static,
+    ) -> future::LocalBoxFuture<'static, U> {
         let mut f = Some(f);
-        self.signal_ref(move |value| {
-            if let Some(value) = value.as_ref() {
-                f.take().unwrap()(value);
-                true
-            } else {
-                false
-            }
-        })
-        .stop_if(|is_some| *is_some)
-        .map(|_| ())
-        .to_future()
-        .boxed_local()
+        self.signal_ref(move |value| value.as_ref().map(|value| f.take().unwrap()(value)))
+            .stop_if(|value| value.is_some())
+            .to_future()
+            .map(|value| value.unwrap())
+            .boxed_local()
     }
 }
 
