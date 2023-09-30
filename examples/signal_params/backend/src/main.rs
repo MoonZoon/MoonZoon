01139cbda8270a9a381823fc339046e9f6781ef7
Martin Kavík <martin@kavik.cz>
2023-09-30 18:25:28 +0200

    example signal_params

---
+++ examples/signal_params/backend/src/main.rs
@@ -0,0 +1,12 @@
+use moon::*;
+
+async fn frontend() -> Frontend {
+    Frontend::new().title("Signal params example")
+}
+
+async fn up_msg_handler(_: UpMsgRequest<()>) {}
+
+#[moon::main]
+async fn main() -> std::io::Result<()> {
+    start(frontend, up_msg_handler, |_| {}).await
+}
