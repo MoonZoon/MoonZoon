3057001449e0cf23707848e0d0dc8f1d32dff19b
Martin Kavík <martin@kavik.cz>
2024-02-25 00:24:35 +0100

    palette, IntoOklchaString

--- examples/counter/frontend/src/main.rs
+++ examples/counter/frontend/src/main.rs
@@ -21,7 +21,15 @@ fn counter_button(label: &str, step: i32) -> impl Element {
         .s(Width::exact(45))
         .s(RoundedCorners::all_max())
         .s(Background::new()
-            .color_signal(hovered_signal.map_bool(|| hsluv!(300, 75, 85), || hsluv!(300, 75, 75))))
+            // color::hex("#124578")
+            // palette::named::WHITE.into_format() ?
+            // .color_signal_new(hovered_signal.map_bool(|| hsluv!(300, 75, 85), || hsluv!(300, 75, 75))))
+            .color_signal_new(hovered_signal.map_bool(|| palette::Oklch::<f32>::new_const(0.5, 0.2, 85.0.into()), || palette::Oklch::new_const(0.8, 0.1, 75.0.into()))))
+            // .color_signal_new(hovered_signal.map_bool(|| "#123569".parse::<palette::Srgb<u8>>().unwrap_throw().into_format(), || "#823569".parse::<palette::Srgb<u8>>().unwrap_throw().into_format())))
+            // .color_signal_new(hovered_signal.map_bool(
+            //     || palette::named::PINK.into_format(), 
+            //     || palette::named::PALEVIOLETRED.into_format()
+            // )))
         .on_hovered_change(move |is_hovered| hovered.set(is_hovered))
         .label(label)
         .on_press(move || *COUNTER.lock_mut() += step)
