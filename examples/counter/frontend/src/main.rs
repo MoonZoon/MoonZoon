48d666c33f3f06539904aced1048de2952900f9a
Martin Kavík <martin@kavik.cz>
2024-03-02 20:56:22 +0100

    crate color_macro

--- examples/counter/frontend/src/main.rs
+++ examples/counter/frontend/src/main.rs
@@ -20,11 +20,12 @@ fn counter_button(label: &str, step: i32) -> impl Element {
     Button::new()
         .s(Width::exact(45))
         .s(RoundedCorners::all_max())
-        .s(Background::new().color_signal(hovered_signal.map_bool(|| "#edc8f5", || "#e1a3ee")))
+        .s(Background::new()
+            .color_signal(hovered_signal.map_bool(|| color!("#edc8f5"), || color!("#E1A3EE", 0.8))))
         .s(Borders::all(
             Border::new()
                 .width(2)
-                .color(oklch().l(0.6).c(0.182).h(350.53).a(1)),
+                .color(color!("oklch(0.6 0.182 350.53 / .7")),
         ))
         .on_hovered_change(move |is_hovered| hovered.set(is_hovered))
         .label(label)
