a641470e95ef4b19127045e770171d8c3fdb0cc2
Martin Kavík <martin@kavik.cz>
2024-03-29 11:33:20 +0100

    tauri_todomvc migrate to v2

--- examples/tauri_todomvc/src-tauri/src/main.rs
+++ examples/tauri_todomvc/src-tauri/src/main.rs
@@ -2,6 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+  // https://github.com/eclipse-platform/eclipse.platform.swt/issues/843
+  // std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+
+  // https://github.com/tauri-apps/tauri/issues/8462
+  std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+  
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
