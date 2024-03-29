1de4bad5f390dfd7a0aeabb8c8d3c429873b1771
Martin Kavík <martin@kavik.cz>
2024-03-29 12:43:06 +0100

    tauri_local_search migrate to v2

--- examples/tauri_local_search/src-tauri/src/main.rs
+++ examples/tauri_local_search/src-tauri/src/main.rs
@@ -2,19 +2,22 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // https://github.com/tauri-apps/tauri/issues/8462
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
     let mut context = tauri::generate_context!();
     let first_window_config = {
-        if context.config_mut().tauri.windows.is_empty() {
+        if context.config_mut().app.windows.is_empty() {
             <_>::default()
         } else {
-            context.config_mut().tauri.windows.remove(0)
+            context.config_mut().app.windows.remove(0)
         }
     };
     tauri::Builder::default()
-        .setup(|app| {
-            tauri::WindowBuilder::from_config(app, first_window_config)
+        .setup(move |app| {
+            tauri::WebviewWindowBuilder::from_config(app, &first_window_config)?
                 .on_web_resource_request(|req, resp| {
-                    if req.uri().starts_with("tauri://") {
+                    if req.uri().scheme().map(|scheme| scheme.as_str() == "tauri").unwrap_or_default() {
                         resp.headers_mut().insert(
                             "Cross-Origin-Opener-Policy",
                             "same-origin".try_into().unwrap(),
@@ -31,3 +34,6 @@ fn main() {
         .run(context)
         .expect("error while running tauri application");
 }
+
+
+// /usr/lib/x86_64-linux-gnu/webkit2gtk-4.1/WebKitNetworkProcess: symbol lookup error: /snap/core20/current/lib/x86_64-linux-gnu/libpthread.so.0: undefined symbol: __libc_pthread_init, version GLIBC_PRIVATE
