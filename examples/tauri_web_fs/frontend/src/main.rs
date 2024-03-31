9008710b784d85aa594b420098f3626d25d662c6
Martin Kavík <martin@kavik.cz>
2024-03-31 22:16:41 +0200

    update Moon deps actix-http/tls, tauri_web_fs experiments

--- examples/tauri_web_fs/frontend/src/main.rs
+++ examples/tauri_web_fs/frontend/src/main.rs
@@ -5,5 +5,35 @@ fn main() {
 }
 
 fn root() -> impl Element {
-    El::new().child("Tauri Web FS")
+    Column::new()
+        .item("Tauri Web FS")
 }
+
+
+
+// @TODO Test `tauri-sys` crate later once Tauri is more mature.
+// @TODO Resolve `url` dependency conflict and ideally get rid of the `url` dependency somehow.
+// @TODO When it works, update docs - the code + the need to install `esbuild`.
+//
+// fn root() -> impl Element {
+//     Column::new()
+//         .item("Tauri Web FS")
+//         .item_signal(signal::from_future(Box::pin(tauri_sys::tauri::invoke("greet", "Jonas"))))
+// }
+
+
+
+// @TODO Test `tauri-bindgen` crate again once both Tauri and the bindgen are more mature.
+//
+// Error: `Fetch API cannot load ipc://localhost/greet/greet. URL scheme "ipc" is not supported.``
+// https://github.com/tauri-apps/tauri-bindgen/issues/147
+//
+// tauri_bindgen_guest_rust::generate!({
+//     path: "../greet.wit"
+// });
+//
+// fn root() -> impl Element {
+//     Column::new()
+//         .item("Tauri Web FS")
+//         .item_signal(signal::from_future(Box::pin(greet::greet("Jonas"))))
+// }
--- examples/tauri_web_fs/src-tauri/src/lib.rs
+++ examples/tauri_web_fs/src-tauri/src/lib.rs
@@ -1,3 +1,12 @@
+#[tauri::command(rename_all = "snake_case")]
+fn greet(name: String) -> String {
+    format!("Hello {name}!")
+}
+
+// window.__TAURI__.core.invoke('greet', { name: 'John' }).then((message) => console.log(message))
+// @TODO fn exchange_channels(channel/jschannelid + webview) -> Channel
+// @TODO document adding `"withGlobalTauri": true,` to `tauri.conf.json`
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     // https://github.com/tauri-apps/tauri/issues/8462
@@ -5,6 +14,47 @@ pub fn run() {
     std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
 
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+
+
+
+// @TODO test tauri-bindgen again once both Tauri and the bindgen are more mature
+
+// use tauri_bindgen_host::ipc_router_wip::{BuilderExt, Router};
+
+// tauri_bindgen_host::generate!({
+//     path: "../greet.wit",
+//     async: false,
+//     tracing: true
+// });
+
+// #[derive(Clone, Copy)]
+// struct GreetCtx;
+
+// impl greet::Greet for GreetCtx {
+//     fn greet(&self, name: String) -> String {
+//         format!(
+//             "Hello, {}! You've been greeted from code-generated Rust!",
+//             name
+//         )
+//     }
+// }
+
+// #[cfg_attr(mobile, tauri::mobile_entry_point)]
+// pub fn run() {
+//     // https://github.com/tauri-apps/tauri/issues/8462
+//     #[cfg(target_os = "linux")]
+//     std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
+//     let mut router: Router<GreetCtx> = Router::new(GreetCtx {});
+//     greet::add_to_router(&mut router, |ctx| ctx).unwrap();
+
+//     tauri::Builder::default()
+//         .ipc_router(router)
+//         .run(tauri::generate_context!())
+//         .expect("error while running tauri application");
+// }
