f7e365f432b6b65c3f58669436fa9694a78c0f7a
Martin Kavík <martin@kavik.cz>
2024-04-02 08:59:10 +0200

    commands.js

---
+++ examples/tauri_ipc/frontend/js/commands.js
@@ -0,0 +1,15 @@
+const invoke = window.__TAURI__.core.invoke;
+
+export async function greet(name) {
+    return await invoke("greet", { name: name });
+}
+
+export async function send_ipc_channel(on_message) {
+    const ipc_channel = new window.__TAURI__.core.Channel();
+    ipc_channel.onmessage = on_message;
+    return await invoke("send_ipc_channel", { channel: ipc_channel });
+}
+
+export async function greet_through_channel(name) {
+    return await invoke("greet_through_channel", { name: name });
+}
