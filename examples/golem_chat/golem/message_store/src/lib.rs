9d41cb41e3029a3fa6e239969d07e65998a253e8
Martin Kavík <martin@kavik.cz>
2024-09-19 17:28:42 +0200

    task::run_all() calls

--- examples/golem_chat/golem/message_store/src/lib.rs
+++ examples/golem_chat/golem/message_store/src/lib.rs
@@ -1,36 +1,56 @@
 mod bindings;
 
 use crate::bindings::exports::golem::component::api::*;
-use std::cell::RefCell;
+use futures_signals::signal::{Mutable, SignalExt};
+use std::sync::LazyLock;
 
-/// This is one of any number of data types that our application
-/// uses. Golem will take care to persist all application state,
-/// whether that state is local to a function being executed or
-/// global across the entire program.
+#[derive(Default)]
 struct State {
-    total: u64,
+    total: Mutable<u64>,
 }
 
-thread_local! {
-    /// This holds the state of our application.
-    static STATE: RefCell<State> = RefCell::new(State {
-        total: 0,
-    });
-}
+static STATE: LazyLock<State> = LazyLock::new(|| {
+    let state = State::default();
+    task::spawn(state.total.signal().for_each(|total| {
+        println!("total changed: {total}!");
+        async {}
+    }));
+    state
+});
 
 struct Component;
 
 impl Guest for Component {
-    /// Updates the component's state by adding the given value to the total.
     fn add(value: u64) {
-        STATE.with_borrow_mut(|state| state.total += value);
-        println!("{value} added!");
+        *STATE.total.lock_mut() += value;
+        task::run_all();
     }
 
-    /// Returns the current total.
     fn get() -> u64 {
-        STATE.with_borrow(|state| state.total)
+        let total = STATE.total.get();
+        task::run_all();
+        total
     }
 }
 
 bindings::export!(Component with_types_in bindings);
+
+mod task {
+    use futures_executor::{LocalPool, LocalSpawner};
+    use futures_task::LocalSpawn;
+    use std::cell::RefCell;
+    use std::future::Future;
+
+    thread_local! {
+        static TASK_POOL: RefCell<LocalPool> = Default::default();
+        static TASK_SPAWNER: LocalSpawner = TASK_POOL.with_borrow(|pool| pool.spawner());
+    }
+
+    pub fn spawn(future: impl Future<Output = ()> + 'static) {
+        TASK_SPAWNER.with(move |spawner| spawner.spawn_local_obj(Box::pin(future).into()).unwrap())
+    }
+
+    pub fn run_all() {
+        TASK_POOL.with_borrow_mut(|pool| pool.run_until_stalled())
+    }
+}
