4d33cd474483e362ec5d131fdb959281ebb6fb28
Martin Kavík <martin@kavik.cz>
2025-02-16 14:32:41 +0100

    resolve_references multi-error handling

--- examples/boon_lang/frontend/src/boon/parser/scope_resolver.rs
+++ examples/boon_lang/frontend/src/boon/parser/scope_resolver.rs
@@ -18,13 +18,12 @@ pub struct Referenceable<'code> {
     level: usize,
 }
 
-pub type ResolveResult<'code, T> = Result<T, Vec<ParseError<'code, Token<'code>>>>;
+pub type ResolveError<'code> = ParseError<'code, Token<'code>>;
 
-// @TODO Return multiple errors
-// @TODO How to resolve loops?
+// @TODO How to handle loops?
 pub fn resolve_references(
     mut expressions: Vec<Spanned<Expression>>,
-) -> ResolveResult<Vec<Spanned<Expression>>> {
+) -> Result<Vec<Spanned<Expression>>, Vec<ResolveError>> {
     let mut reachable_referenceables = ReachableReferenceables::default();
     let level = 0;
     for mut expressions in &expressions {
@@ -34,21 +33,27 @@ pub fn resolve_references(
             reachable_referenceables.insert(name, Referenceable { name, span: *span, level });
         }
     }
+    let mut errors = Vec::new();
     for expression in &mut expressions {
-        set_reference_count_and_alias_referenceables(expression, reachable_referenceables.clone(), level)?
+        set_reference_count_and_alias_referenceables(expression, reachable_referenceables.clone(), level, &mut errors);
     }
-    Ok(expressions)
+    if errors.is_empty() {
+        Ok(expressions)
+    } else {
+        Err(errors)
+    }
 }
 
 fn set_reference_count_and_alias_referenceables<'a, 'code>(
     mut expression: &'a mut Spanned<Expression<'code>>,
     mut reachable_referenceables: ReachableReferenceables<'code>,
     mut level: usize,
-) -> ResolveResult<'code, ()> {
+    errors: &mut Vec<ResolveError>,
+) {
     let Spanned { span, node: expression } = &mut expression;
     match expression {
         Expression::Variable(variable) => {
-            set_reference_count_and_alias_referenceables(&mut variable.value, reachable_referenceables, level)?;
+            set_reference_count_and_alias_referenceables(&mut variable.value, reachable_referenceables, level, errors);
         }
         Expression::Object(object) => {
             level += 1;
@@ -59,7 +64,7 @@ fn set_reference_count_and_alias_referenceables<'a, 'code>(
             }
             for mut variable in &mut object.variables {
                 let Spanned { span, node: variable } = variable;
-                set_reference_count_and_alias_referenceables(&mut variable.value, reachable_referenceables.clone(), level)?;
+                set_reference_count_and_alias_referenceables(&mut variable.value, reachable_referenceables.clone(), level, errors);
             }
         }
         Expression::TaggedObject { tag, object } => {
@@ -71,7 +76,7 @@ fn set_reference_count_and_alias_referenceables<'a, 'code>(
             }
             for mut variable in &mut object.variables {
                 let Spanned { span, node: variable } = variable;
-                set_reference_count_and_alias_referenceables(&mut variable.value, reachable_referenceables.clone(), level)?;
+                set_reference_count_and_alias_referenceables(&mut variable.value, reachable_referenceables.clone(), level, errors);
             }
         }
         Expression::FunctionCall { path, arguments } => {
@@ -84,7 +89,7 @@ fn set_reference_count_and_alias_referenceables<'a, 'code>(
             for mut argument in arguments {
                 let Spanned { span, node: argument } = argument;
                 if let Some(value) = argument.value.as_mut() {
-                    set_reference_count_and_alias_referenceables(value, reachable_referenceables.clone(), level)?;
+                    set_reference_count_and_alias_referenceables(value, reachable_referenceables.clone(), level, errors);
                 }
             }
         }
@@ -97,94 +102,89 @@ fn set_reference_count_and_alias_referenceables<'a, 'code>(
             }
             for mut variable in variables {
                 let Spanned { span, node: variable } = variable;
-                set_reference_count_and_alias_referenceables(&mut variable.value, reachable_referenceables.clone(), level)?;
+                set_reference_count_and_alias_referenceables(&mut variable.value, reachable_referenceables.clone(), level, errors);
             }
-            set_reference_count_and_alias_referenceables(output, reachable_referenceables, level)?;
+            set_reference_count_and_alias_referenceables(output, reachable_referenceables, level, errors);
         }
         Expression::List { items } => {
             for mut item in items {
-                set_reference_count_and_alias_referenceables(item, reachable_referenceables.clone(), level)?;
+                set_reference_count_and_alias_referenceables(item, reachable_referenceables.clone(), level, errors);
             }
         }
         Expression::Map { entries } => {
             // @TODO implement, see the error message below
-            Err(vec![ParseError::custom(*span, "Scope resolver cannot resolve references in Expression::Map yet, sorry".to_owned())])?
+            errors.push(ResolveError::custom(*span, "Scope resolver cannot resolve references in Expression::Map yet, sorry".to_owned()))
         }
         Expression::Latest { inputs } => {
             for mut input in inputs {
-                set_reference_count_and_alias_referenceables(input, reachable_referenceables.clone(), level)?;
+                set_reference_count_and_alias_referenceables(input, reachable_referenceables.clone(), level, errors);
             }
         }
         Expression::Then { body } => {
-            set_reference_count_and_alias_referenceables(body, reachable_referenceables, level)?;
+            set_reference_count_and_alias_referenceables(body, reachable_referenceables, level, errors);
         }
         Expression::When { arms } => {
             // @TODO implement, see the error message below
-            Err(vec![ParseError::custom(*span, "Scope resolver cannot resolve references in Expression::When yet, sorry".to_owned())])?
+            errors.push(ResolveError::custom(*span, "Scope resolver cannot resolve references in Expression::When yet, sorry".to_owned()))
         }
         Expression::While { arms } => {
             // @TODO implement, see the error message below
-            Err(vec![ParseError::custom(*span, "Scope resolver cannot resolve references in Expression::While yet, sorry".to_owned())])?
+            errors.push(ResolveError::custom(*span, "Scope resolver cannot resolve references in Expression::While yet, sorry".to_owned()))
         }
         Expression::Pipe { from, to } => {
-            set_reference_count_and_alias_referenceables(from, reachable_referenceables.clone(), level)?;
-            set_reference_count_and_alias_referenceables(to, reachable_referenceables, level)?;
+            set_reference_count_and_alias_referenceables(from, reachable_referenceables.clone(), level, errors);
+            set_reference_count_and_alias_referenceables(to, reachable_referenceables, level, errors);
         }
         Expression::ArithmeticOperator(_) => {
             // @TODO implement, see the error message below
-            Err(vec![ParseError::custom(*span, "Scope resolver cannot resolve references in Expression::ArithmeticOperator yet, sorry".to_owned())])?
+            errors.push(ResolveError::custom(*span, "Scope resolver cannot resolve references in Expression::ArithmeticOperator yet, sorry".to_owned()))
         },
         Expression::Comparator(_) => {
             // @TODO implement, see the error message below
-            Err(vec![ParseError::custom(*span, "Scope resolver cannot resolve references in Expression::Comparator yet, sorry".to_owned())])?
+            errors.push(ResolveError::custom(*span, "Scope resolver cannot resolve references in Expression::Comparator yet, sorry".to_owned()))
         },
         Expression::Function { .. } => {
             // @TODO implement, see the error message below
-            Err(vec![ParseError::custom(*span, "Scope resolver cannot resolve references in Expression::Function yet, sorry".to_owned())])?
+            errors.push(ResolveError::custom(*span, "Scope resolver cannot resolve references in Expression::Function yet, sorry".to_owned()))
         },
         Expression::Alias(alias) => {
-            set_referenced_referenceable(alias, *span, reachable_referenceables)?
+            set_referenced_referenceable(alias, *span, reachable_referenceables, errors)
         }
         Expression::LinkSetter { alias } =>  {
             let Spanned { span, node: alias } = alias;
-            set_referenced_referenceable(alias, *span, reachable_referenceables)?
+            set_referenced_referenceable(alias, *span, reachable_referenceables, errors)
         },
         Expression::Literal(_) => (),
         Expression::Link => (),
         Expression::Skip => (),
     }
-    Ok(())
 }
 
 fn set_referenced_referenceable<'code>(
     alias: &mut Alias<'code>, 
     span: Span, 
-    reachable_referenceables: ReachableReferenceables<'code>
-) -> ResolveResult<'code, ()> {
+    reachable_referenceables: ReachableReferenceables<'code>,
+    errors: &mut Vec<ResolveError>,
+) {
     match alias {
-        Alias::WithPassed { extra_parts } => Ok(()),
+        Alias::WithPassed { extra_parts } => (),
         Alias::WithoutPassed { parts, referenceables: unset_referenceables } => {
             // @TODO make the first part a standalone property (name or PASSED)?
             let first_part = parts.first().expect("Failed to get first alias part");
             // @TODO make Argument name optional to model the case when an argument is piped better?
             if first_part.is_empty() {
-                return Ok(())
+                return;
             }
             let referenced = reachable_referenceables.get(first_part).copied();
-            let error = referenced.is_none().then(|| {
+            if referenced.is_none() {
                 let reachable_names = reachable_referenceables.keys();
-                vec![ParseError::custom(span, format!("Cannot find the variable or argument '{first_part}'. You can refer to: {reachable_names:?}"))]
-            });
+                errors.push(ResolveError::custom(span, format!("Cannot find the variable or argument '{first_part}'. You can refer to: {reachable_names:?}")))
+            }
             let referenceables = Referenceables {
                 referenced,
                 reachable: reachable_referenceables,
             };
             *unset_referenceables = Some(referenceables);
-            if let Some(error) = error {
-                Err(error)
-            } else {
-                Ok(())
-            }
         }
     }
 }
