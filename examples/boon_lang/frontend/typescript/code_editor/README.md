dc9ed9c3610a142d0e9a47f65d312ef04cb25d05
Martin Kavík <martin@kavik.cz>
2025-02-17 18:06:52 +0100

    code_editor

---
+++ examples/boon_lang/frontend/typescript/code_editor/README.md
@@ -0,0 +1,12 @@
+Init
+- `npm install`
+
+Build & Watch
+- `./node_modules/.bin/rolldown code_editor.ts --file ../bundles/code_editor.js --watch`
+
+Created with commands:
+- `npm install -D rolldown` (`tested with v1.0.0-beta.3`)
+- `npm i -E codemirror`
+- `npm i -E @codemirror/theme-one-dark`
+- `npm i -E @codemirror/view`
+- `npm i -E @codemirror/state`
---
+++ examples/boon_lang/frontend/typescript/code_editor/code_editor.ts
@@ -0,0 +1,34 @@
+import { EditorState, Compartment, Extension } from '@codemirror/state'
+import { EditorView } from '@codemirror/view'
+import { basicSetup } from 'codemirror'
+import { oneDark } from '@codemirror/theme-one-dark'
+
+export class CodeEditorController {
+    constructor() {}
+
+    editor_view: EditorView | null = null
+    file_content = new Compartment()
+
+    set_content(content: string) {
+        this.editor_view!.dispatch({
+            changes: [
+                { from: 0, to: this.editor_view!.state.doc.length },
+                { from: 0, insert: content },
+            ]
+        })
+    }
+
+    async init(parent_element: HTMLElement) {
+        const state = EditorState.create({
+            extensions: [
+                basicSetup,
+                oneDark,
+            ],
+        })
+
+        this.editor_view = new EditorView({
+            parent: parent_element,
+            state
+        });
+    }
+}
